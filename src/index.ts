e3caecdc9c6519e6676ce4010ba8911ec80d448b
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-02-28 21:00:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { getRoute } from './data/apis/getRoute.ts';
 
 window.bus = {};
 window.bus.initialize = function () {};
+window.bus.getRoute = getRoute
 window.bus.integrateRoute = integrateRoute;
 window.bus.test = function () {
   getRoute().then((e) => {
+console.log(e)
     var rf = {};
     for (var o in e) {
       if (String(e[o].n).indexOf('236')>-1) {
         rf.id = parseInt(o.split('_')[1]);
         rf.pid = e[o].pid;
+console.log(e[o])
       }
       break;
     }
