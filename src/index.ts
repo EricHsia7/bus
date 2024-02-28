7cf971695160fe666d9d4843cabd270394692116
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-02-28 20:46:43 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { integrateRoute } from './data/index.ts';
+import { getRoute } from './data/apis/getRoute.ts';
 
 window.bus = {};
 window.bus.initialize = function () {};
 window.bus.integrateRoute = integrateRoute;
 window.bus.test = function () {
-  integrateRoute(16406, [157402]).then((f) => {
-    console.log(f);
+  getRoute().then((e) => {
+    var rf = {};
+    for (var o in e) {
+      if (e[o].n === '236') {
+        rf.id = n.split('_')[1];
+        rf.pid = e[o].pid;
+      }
+      break;
+    }
+    integrateRoute(rf.id, rf.pid).then((f) => {
+      console.log(f);
+    });
   });
 };
 export default window.bus;
