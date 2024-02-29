9b71cc7b06c3eec12b1c5bb912c846c4685f2460
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-02-29 16:27:29 +0800

    Update route.ts

--- src/interface/route.ts
+++ src/interface/route.ts
@@ -2,10 +2,10 @@ import { integrateRoute } from '../data/index.ts';
 
 function setUpRouteField(Field: HTMLElement) {}
 
-export async function formatRoute(Field: HTMLElement, RouteID: number, PathAttributeId: number) {
+export async function formatRoute(RouteID: number, PathAttributeId: number) {
   function formatEstimateTime(EstimateTime: string, mode: number = 1) {
-    var time = parseInt(EstimateTime);
-    if (mode === 0) {
+function formatTime(time: number, mode: number) {
+if (mode === 0) {
       return `${time}秒`;
     }
     if (mode === 1) {
@@ -17,13 +17,44 @@ export async function formatRoute(Field: HTMLElement, RouteID: number, PathAttri
       var minutes = String(Math.floor(time / 60));
       return `${minutes}分`;
     }
+return 'Invaild mode'
+}
+    var time = parseInt(EstimateTime);
+if (time === -3) {
+      return { code: 6, text: '末班駛離' }
+    }
+    if (time === -4) {
+      return { code: 5, text: '今日停駛' }
+    }
+    if (time === -2) {
+      return { code: 4, text: '交通管制' }
+    }
+    if (time === -1) {
+      return { code: 3, text: '未發車' }
+    }
+    if (time <= 180) {
+      if (t <= 100) {
+          if (t <= 10) {
+            return { code: 2, text: '進站中' }
+          }
+          else {
+            return { code: 2, text: formatTime(time,mode)}
+          }
+        }
+      else {
+        return { code: 1, text: formatTime(time,mode) }
+      }
+    }
+    else {
+      return { code: 0, text: formatTime(time,mode)}
+    }
   }
   var integration = await integrateRoute(RouteID, PathAttributeId);
   var groupedItems = {};
   for (var item of integration.items) {
     var formattedItem = {};
     formattedItem.name = item.hasOwnProperty('_Stop') ? item._Stop.nameZh : 'No name to display';
-    formattedItem.time = formatEstimateTime(item.EstimateTime);
+    formattedItem.status = formatEstimateTime(item.EstimateTime);
     formattedItem.bus = item.hasOwnProperty('_BusEvent') ? item._BusEvent : null;
     formattedItem.sequence = item.hasOwnProperty('_Stop') ? item._Stop.seqNo : -1;
     formattedItem.location = {
