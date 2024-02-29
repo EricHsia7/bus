790c82d2759ffb0ec56cf45e2df4de356726dc43
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-02-29 19:32:15 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,31 @@ import { integrateRoute } from './data/apis/index.ts';
 import { getRoute } from './data/apis/getRoute.ts';
 import { searchRoute } from './data/search/searchRoute.ts';
 import { displayRoute } from './interface/route.ts';
+
+
+//for development
+
+const ErrorStackParser = require('error-stack-parser');
+const StackTrace = require('stacktrace-js');
+
+window.onerror = async function (message, source, lineno, colno, error) {
+  StackTrace.fromError(error).then(function (stackTrace) {
+    var parsedStackTrace = stackTrace.map(function (frame) {
+      return {
+        functionName: frame.functionName,
+        fileName: frame.fileName,
+        lineNumber: frame.lineNumber,
+        columnNumber: frame.columnNumber
+      };
+    });
+    console.log('%c ----------', 'color: #888;');
+    parsedStackTrace.forEach((e) => {
+      console.error(`func: ${e.functionName}\npath: ${e.fileName}\nlocation: L${e.lineNumber} C${e.columnNumber}`);
+    });
+  });
+};
+
+
 window.bus = {};
 window.bus.initialize = function () {};
 window.bus.getRoute = getRoute;
