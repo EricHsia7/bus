e7e12087df8a3685bfbc922241665101c0e7e84d
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-06 13:25:19 +0800

    Update index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,20 @@
-export function compareString(a, b) {
+var md5 = require('md5');
 
-
+export function compareString(a: string, b: string): boolean{
+var hash_a = md5(a);
+var hash_b = md5(b);
+const length = 32
+for(var i = 0; i < length; i++) {
+var a_i = a.charAt(i)
+var b_i = b.charAt(i)
+var equal = true
+if(a_i===b_i) {
+continue;
+}
+else {
+equal = false
+break;
+}
+return equal
+}
 }
