31b8517cf580fe0bb1feee7ab57848b62a091c6c
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-06 14:26:09 +0800

    Update index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,9 +1,31 @@
 var md5 = require('md5');
 
-export function compareThings(a: string, b: string): boolean{
-var hash_a = md5(a);
-var hash_b = md5(b);
+export function compareThings(a: any, b: any): boolean{
 const length = 32
+if(typeof a === 'object') {
+var ax = JSON.stringify(a)
+}
+if(typeof b === 'object') {
+var bx = JSON.stringify(b)
+}
+
+if(typeof a === 'number') {
+var ax = String(a)
+}
+if(typeof b === 'number') {
+var bx = String(b)
+}
+
+if(typeof a === 'boolean') {
+var ax = String(a)
+}
+if(typeof b === 'boolean') {
+var bx = String(b)
+}
+if(ax.length > length || bx.length > length) {
+var hash_a = md5(ax);
+var hash_b = md5(bx);
+
 for(var i = 0; i < length; i++) {
 var a_i = a.charAt(i)
 var b_i = b.charAt(i)
@@ -15,6 +37,15 @@ else {
 equal = false
 break;
 }
+}
 return equal
+}
+else {
+if (ax === bx) {
+return true
+}
+else {
+return false
+}
 }
 }
