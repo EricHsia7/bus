0c71cf9289bcb5e02fcc50bb3a2cba86baddec81
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-06 23:20:31 +0800

    update

--- src/data/apis/loader.ts
+++ src/data/apis/loader.ts
@@ -19,7 +19,7 @@ export async function fetchData(url: string, requestID: string, urlName: string)
     }
     chunks.push(value);
     receivedLength += value.length;
-    setDataReceivingProgress(requestID, urlName, receivedLength / contentLength);
+    setDataReceivingProgress(requestID, urlName, receivedLength / contentLength, false);
   }
 
   // Concatenate all the chunks into a single Uint8Array
@@ -66,14 +66,15 @@ export function setDataReceivingProgress(requestID: string, urlName: string, pro
     } else {
       dataReceivingProgress[requestID][key].expel = false;
       var change = 0;
-      var change = progress - dataReceivingProgress[requestID][key].progress;
+      var change = progress - dataReceivingProgress[requestID][key].previous_progress;
       if (change < 0) {
         dataReceivingProgress[requestID][key].total = dataReceivingProgress[requestID][key].total + 1;
       }
       dataReceivingProgress[requestID][key].progress = dataReceivingProgress[requestID][key].progress + Math.abs(change);
+      dataReceivingProgress[requestID][key].previous_progress = progress;
     }
   } else {
-    dataReceivingProgress[requestID][key] = { progress: progress, total: 1, expel: false };
+    dataReceivingProgress[requestID][key] = { progress: progress, total: 1, previous_progress: 0, expel: false };
   }
-  console.log(dataReceivingProgress)
+  console.log(dataReceivingProgress);
 }
