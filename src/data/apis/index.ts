6fd28e0bd4696eb9cf6e8b7d4a6c9b581d3a2233
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-14 22:44:04 +0800

    update

--- src/data/apis/index.ts
+++ src/data/apis/index.ts
@@ -125,12 +125,15 @@ function processEstimateTime(EstimateTime: object, Stop: object, Location: objec
   });
   var result2 = [];
   var endpointCount = 0;
+  var multipleEndpoints = segmentBuffer.length % 2 === 0 ? true : false;
   for (var item of result) {
-    if (item._segmentBuffer) {
-      endpointCount += 1;
-    }
-    if (endpointCount % 2 === 1) {
-      item._segmentBuffer = true;
+    if (multipleEndpoints) {
+      if (item._segmentBuffer) {
+        endpointCount += 1;
+      }
+      if (endpointCount % 2 === 1) {
+        item._segmentBuffer = true;
+      }
     }
     result2.push(item);
   }
