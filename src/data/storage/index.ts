82d911e5581ef4c6d26c277b07d2cce9ac97b288
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-22 20:03:31 +0800

    update

--- src/data/storage/index.ts
+++ src/data/storage/index.ts
@@ -1,24 +1,30 @@
 const localforage = require('localforage');
 const storage = {
-  cacheStore: localforage.createInstance({
-    name: 'busCache'
-  }),
-  settingsStore: localforage.createInstance({
-    name: 'busSettings'
-  }),
-  analyticsStore: localforage.createInstance({
-    name: 'busAnalytics'
-  })
+  cacheStore: false,
+  settingsStore: false,
+  analyticsStore: false
 };
 var stores = ['cacheStore', 'settingsStore', 'analyticsStore'];
 async function dropInstance(store: number): any {
+  var key = stores[store];
+  if (storage[key] === false) {
+    storage[key] = localforage.createInstance({
+      name: key
+    });
+  }
   var operation = await storage[stores[store]].dropInstance();
   return operation;
 }
 
 export async function lfSetItem(store: number, key: string, value: any): any {
   try {
-    var operation = await storage[stores[store]].setItem(key, value);
+    var key = stores[store];
+    if (storage[key] === false) {
+      storage[key] = localforage.createInstance({
+        name: key
+      });
+    }
+    var operation = await storage[key].setItem(key, value);
     return operation;
   } catch (err) {
     console.log(err);
@@ -29,6 +35,12 @@ export async function lfSetItem(store: number, key: string, value: any): any {
 
 export async function lfGetItem(store: number, key: string): any {
   try {
+    var key = stores[store];
+    if (storage[key] === false) {
+      storage[key] = localforage.createInstance({
+        name: key
+      });
+    }
     var operation = await storage[stores[store]].getItem(key);
     return operation;
   } catch (err) {
