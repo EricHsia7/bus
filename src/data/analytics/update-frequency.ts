7bad67d2dddd85d81f24fd6c494b1b559574e65c
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-03-23 20:09:19 +0800

    update

--- src/data/analytics/update-frequency.ts
+++ src/data/analytics/update-frequency.ts
@@ -40,14 +40,79 @@ export async function recordEstimateTime(EstimateTime: object): void {
   }
 }
 
-export async function listRecordedEstimateTime() {
+export async function listRecordedEstimateTime(): [] {
   var keys = await lfListItem(3);
+  var result = [];
   for (var key of keys) {
     var json = await lfGetItem(3, key);
     var object = JSON.parse(json);
     for (var key2 in object) {
-      console.log(key, key2);
-      console.log(object[key2].map((item) => `${item.EstimateTime} ${item.timeStamp}`).join(`\n`));
+      result.push(object[key2].map((item) => [item.EstimateTime, item.timeStamp]));
+    }
+  }
+  return result;
+}
+
+// Function to split data based on delta
+function splitDataByDelta(data: []): [] {
+  const result = [];
+  let currentGroup = [];
+  for (let i = 0; i < data.length; i++) {
+    if (i === 0 || data[i][0] - data[i - 1][0] > 0) {
+      if (currentGroup.length > 0) {
+        result.push(currentGroup);
+      }
+      currentGroup = [data[i]];
+    } else {
+      currentGroup.push(data[i]);
+    }
+  }
+  if (currentGroup.length > 0) {
+    result.push(currentGroup);
+  }
+  return result;
+}
+
+// Function to calculate Pearson correlation coefficient
+function pearsonCorrelation(x, y) {
+  const n = x.length;
+  if (n !== y.length) {
+    throw new Error('Arrays must have the same length');
+  }
+
+  let sumX = 0,
+    sumY = 0,
+    sumXY = 0,
+    sumXSquared = 0,
+    sumYSquared = 0;
+
+  for (let i = 0; i < n; i++) {
+    sumX += x[i];
+    sumY += y[i];
+    sumXY += x[i] * y[i];
+    sumXSquared += x[i] ** 2;
+    sumYSquared += y[i] ** 2;
+  }
+
+  const numerator = n * sumXY - sumX * sumY;
+  const denominator = Math.sqrt((n * sumXSquared - sumX ** 2) * (n * sumYSquared - sumY ** 2));
+
+  if (denominator === 0) {
+    return 0; // Correlation is undefined in this case
+  }
+
+  return numerator / denominator;
+}
+
+export async function processRecordedEstimateTime() {
+  var collection = await listRecordedEstimateTime();
+  for (var dataSet of collection) {
+    var groups = splitDataByDelta(dataSet);
+    for (var group of groups) {
+      const firstColumn = group.map((item) => item[0]);
+      const secondColumn = group.map((item) => item[1]);
+      const correlation = pearsonCorrelation(firstColumn, secondColumn);
+      console.log('Correlation:', correlation);
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { integrateRoute } from './data/apis/index.ts';
 import { getRoute } from './data/apis/getRoute.ts';
 import { searchRouteByName } from './data/search/searchRoute.ts';
 import { calculateDataUsage } from './data/analytics/data-usage.ts';
-import { listRecordedEstimateTime } from './data/analytics/update-frequency.ts';
+import { listRecordedEstimateTime, processRecordedEstimateTime } from './data/analytics/update-frequency.ts';
 import { displayRoute, updateRouteField, formatRoute, openRoute, closeRoute, switchRoute, stretchItemBody, initializeRouteSliding, openRouteByURLScheme, ResizeRouteField, switchRouteBodyTab } from './interface/route.ts';
 import { openSearchPage, closeSearchPage } from './interface/search-page/index.ts';
 import { typeTextIntoInput, deleteCharFromInout, emptyInput } from './interface/search-page/keyboard.ts';
@@ -64,7 +64,8 @@ window.bus = {
   },
   test: {
     calculateDataUsage,
-    listRecordedEstimateTime
+    listRecordedEstimateTime,
+    processRecordedEstimateTime
   }
 };
 window.bus.searchRouteByName = searchRouteByName;
