274bee9e0fbfc059ffe86fc98a5a0def5aeecdd2
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-05 18:14:45 +0800

    update

--- src/interface/settings/options.ts
+++ src/interface/settings/options.ts
@@ -1,15 +1,23 @@
 import { md5 } from '../../tools/index.ts';
 import { getSettingOptions } from '../../data/settings/index.ts';
 
-
-function initializeSettingsOptionsField(Field: HTMLElement, settingKey: string): void {
-
+function generateElementOfItem(item: object, index: number): object {
+  var identifier = `i_${md5(Math.random() + new Date().getTime())}`;
+  var element = document.createElement('div');
+  element.classList.add('option');
+  element.id = identifier;
+  element.setAttribute('type', item.type);
+  element.innerHTML = `<div class="option_name">${item.name}</div><div class="option_checkbox"><input type="checkbox" ${item.option === index ? 'checked' : ''}/></div>`;
+  return {
+    element: element,
+    id: identifier
+  };
 }
+
+function initializeSettingsOptionsField(Field: HTMLElement, settingKey: string): void {}
 
 export function openSettingsOptionsPage(settingKey: string): void {
   var Field = document.querySelector('.settings_options_page_field');
 }
 
-export function closeSettingsOptionsPage(): void {
-
-}
+export function closeSettingsOptionsPage(): void {}
