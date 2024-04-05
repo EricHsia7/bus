840bf7b96896f3b2677f83a8b3412839f42e49bc
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-06 00:14:57 +0800

    update

--- src/data/settings/index.ts
+++ src/data/settings/index.ts
@@ -2,7 +2,7 @@ import { lfSetItem, lfGetItem, lfListItem } from '../storage/index.ts';
 import { formatTime } from '../../tools/format-time.ts';
 import { getHTMLVersionHash } from './version.ts';
 
-const SettingKeys = ['time_formatting_mode', 'display_current_location', 'refresh_interval'];
+const SettingKeys = ['time_formatting_mode', 'refresh_interval', 'display_current_location'];
 
 var Settings = {
   time_formatting_mode: {
