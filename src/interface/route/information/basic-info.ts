8e0efe1e2f0925e65740f83c40dbffb2340fb862
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-07 21:25:31 +0800

    update

---
+++ src/interface/route/information/basic-info.ts
@@ -0,0 +1,6 @@
+function generateElementOfProperty(item) {
+/* <div class="route_information_property" property="">
+  <div class="route_information_property_icon"></div>
+  <div class="route_information_property_value"></div>
+</div>*/
+}
