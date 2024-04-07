dc89c54bccd992d8e1dfa5a16badbc9c050225a9
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-07 16:39:11 +0800

    update

--- src/data/apis/index.ts
+++ src/data/apis/index.ts
@@ -351,10 +351,10 @@ export async function integrateRouteInformation(RouteID: number, PathAttributeId
           var maxWindow = parseInt(item.LowHeadway);
           var averageWindow = (maxWindow + minWindow) / 2;
 
-          var headwayQuantity = thisPeriodDurationInMinutes / averageWindow;
+          var headwayQuantity = thisPeriodDurationInMinutes / minWindow;
           for (var i = 0; i < headwayQuantity; i++) {
             var violateRules = false;
-            var thisHeadwayDate = offsetDate(thisDayOrigin, 0, thisPeriodStartTime.hours, thisPeriodStartTime.minutes + averageWindow * i);
+            var thisHeadwayDate = offsetDate(thisDayOrigin, 0, thisPeriodStartTime.hours, thisPeriodStartTime.minutes + minWindow * i);
             if (thisHeadwayDate.getTime() < thisPeriodStartTimeDateObject.getTime()) {
               violateRules = true;
             }
