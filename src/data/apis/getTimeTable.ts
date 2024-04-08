1b7c2fd5a2435f4e7a41e9f1fb0748a7cd0385c5
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-08 22:08:48 +0800

    Update getTimeTable.ts

--- src/data/apis/getTimeTable.ts
+++ src/data/apis/getTimeTable.ts
@@ -1,16 +1,52 @@
 import { getAPIURL } from './getURL.ts';
 import { fetchData, setDataUpdateTime } from './loader.ts';
+var TimetableAPIVariableCache = {
+available: false,
+data:null
+}
 
-export async function getTimeTable(requestID: string): [] {
-  var apis = [
-    [0, 14],
-    [1, 14]
-  ].map((e) => getAPIURL(e[0], e[1], 8 * 60 * 60 * 1000));
-  var result = [];
-  for (var api of apis) {
-    var data = await fetchData(api, requestID, 'getTimeTable');
-    result = result.concat(data.BusInfo);
-    setDataUpdateTime(requestID, data.EssentialInfo.UpdateTime);
+export async function getTimeTable(requestID: string): object {
+  async function getData() {
+    var apis = [
+      [0, 14],
+      [1, 14]
+    ].map((e) => getAPIURL(e[0], e[1], 60 * 60 * 1000));
+    var result = [];
+    for (var api of apis) {
+      var data = await fetchData(api, requestID, 'getTimeTable');
+      result = result.concat(data.BusInfo);
+      setDataUpdateTime(requestID, data.EssentialInfo.UpdateTime);
+    }
+    return result;
   }
-  return result;
+
+  var cache_time = 60 * 60 * 24 * 14 * 1000;
+  var cache_key = 'bus_timetable_cache';
+  var cached_time = await lfGetItem(0, `${cache_key}_timestamp`);
+  if (cached_time === null) {
+    var result = await getData();
+    await lfSetItem(0, `${cache_key}_timestamp`, new Date().getTime());
+    await lfSetItem(0, `${cache_key}`, JSON.stringify(result));
+    if (!TimetableAPIVariableCache.available) {
+      TimetableAPIVariableCache.available = true;
+      TimetableAPIVariableCache.data = result;
+    }
+    return result;
+  } else {
+    if (new Date().getTime() - parseInt(cached_time) > cache_time) {
+      var result = await getData();
+      await lfSetItem(0, `${cache_key}_timestamp`, new Date().getTime());
+      await lfSetItem(0, `${cache_key}`, JSON.stringify(result));
+      return result;
+    } else {
+      if (!TimetableAPIVariableCache.available) {
+        var cache = await lfGetItem(0, `${cache_key}`);
+        TimetableAPIVariableCache.available = true;
+        TimetableAPIVariableCache.data = JSON.parse(cache);
+      }
+      setDataReceivingProgress(requestID, 'getStop', 0, true);
+      setDataUpdateTime(requestID, -1);
+      return TimetableAPIVariableCache.data;
+    }
+  }
 }
