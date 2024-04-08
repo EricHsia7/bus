35f28c04c3eb061fc935262f82f22315ebe2613f
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-08 10:11:13 +0800

    Update calendar.ts

--- src/interface/route/information/calendar.ts
+++ src/interface/route/information/calendar.ts
@@ -129,7 +129,7 @@ export function setUpEventsGroupFieldSkeletonScreen(Field: HTMLElement) {
       });
     }
   }
-  updateEventsGroupField(
+  updateCalendarField(
     Field,
     {
       foldedEvents: foldedEvents,
