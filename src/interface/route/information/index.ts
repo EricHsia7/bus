135db9f7071b6aeb1802bcf37082bf4032e4fb99
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-10 18:22:02 +0800

    update

--- src/interface/route/information/index.ts
+++ src/interface/route/information/index.ts
@@ -1,13 +1,17 @@
 import { integrateRouteInformation } from '../../../data/apis/index.ts';
 import { initializeCalendarGridlines, setUpCalendarFieldSkeletonScreen, updateCalendarField } from './calendar.ts';
+import { setUpPropertiesFieldSkeletonScreen, updatePropertiesField } from './properties.ts';
 import { md5 } from '../../../tools/index.ts';
 
 async function initializeRouteInformationField(Field: HTMLElement, RouteID: number, PathAttributeId: [number]): void {
+  var propertiesField = Field.querySelector('.route_information_body .route_information_groups .route_information_group[group="properties"]');
   var calendarField = Field.querySelector('.route_information_body .route_information_groups .route_information_group[group="calendar"]');
+  setUpPropertiesFieldSkeletonScreen(propertiesField)
   initializeCalendarGridlines(calendarField);
   setUpCalendarFieldSkeletonScreen(calendarField);
   const requestID = `r_${md5(Math.random() * new Date().getTime())}`;
   var integration = await integrateRouteInformation(RouteID, PathAttributeId, requestID);
+  updatePropertiesField(propertiesField,integration)
   updateCalendarField(calendarField, integration.calendar, false);
 }
 
--- src/interface/route/information/properties.ts
+++ src/interface/route/information/properties.ts
@@ -1,7 +1,9 @@
 import { GeneratedElement } from '../../index.ts';
 import { md5 } from '../../../tools/index.ts';
 
-function queryBasicInfoFieldSize(): object {
+var previousProperties = {};
+
+function queryPropertiesFieldSize(): object {
   return {
     width: window.innerWidth,
     height: window.innerHeight
@@ -20,39 +22,52 @@ function generateElementOfProperty(): GeneratedElement {
   };
 }
 
-function updateBasicInfoField(Field: HTMLElement, skeletonScreen: boolean) {
-  function updateProperty(thisElement: HTMLElement, thisProperty: object, previousProperty: object): void {
+export function setUpPropertiesFieldSkeletonScreen(Field: HTMLElement) {
+  const FieldSize = queryPropertiesFieldSize();
+  const FieldWidth = FieldSize.width;
+  const FieldHeight = FieldSize.height;
+  var defaultPropertyQuantity = 5;
+  var properties = [];
+  for (var i = 0; i < defaultPropertyQuantity; i++) {
+    properties.push({
+      key: 'i',
+      icon: 'none',
+      value: ''
+    });
+  }
+  updateCalendarField(Field, properties, true);
+}
+
+export function updatePropertiesField(Field: HTMLElement, properties: [], skeletonScreen: boolean) {
+  function updateProperty(thisElement: HTMLElement, thisProperty: object, previousProperties: object): void {
     function updateIcon(thisElement: HTMLElement, thisProperty: object): void {
       thisElement.querySelector('.route_information_property_icon').innerHTML = thisProperty.icon;
     }
     function updateValue(thisElement: HTMLElement, thisProperty: object): void {
       thisElement.querySelector('.route_information_property_value').innerText = thisProperty.value;
     }
-    if (previousProperty === null) {
+    if (previousProperties === null) {
       updateProperty(thisElement, thisProperty);
       updateValue(thisElement, thisProperty);
     } else {
-      if (!compareThings(previousProperty, thisProperty)) {
+      if (!compareThings(previousProperties, thisProperty)) {
         updateProperty(thisElement, thisProperty);
       }
-      if (!compareThings(previousProperty, thisProperty)) {
+      if (!compareThings(previousProperties, thisProperty)) {
         updateValue(thisElement, thisProperty);
       }
     }
   }
 
-  const FieldSize = queryBasicInfoFieldSize();
+  const FieldSize = queryPropertiesFieldSize();
   const FieldWidth = FieldSize.width;
   const FieldHeight = FieldSize.height;
 
-  if (previousCalendar === {}) {
-    previousCalendar = calendar;
+  if (previousProperties === {}) {
+    previousProperties = properties;
   }
 
-  var eventGroupQuantity = calendar.eventGroupQuantity;
-  var eventQuantity = calendar.eventQuantity;
-  var groupedEvents = calendar.groupedEvents;
-  var eventGroups = calendar.eventGroups;
+  var propertyQuantity = properties.length;
 
   Field.setAttribute('skeleton-screen', skeletonScreen);
 
@@ -62,16 +77,13 @@ function updateBasicInfoField(Field: HTMLElement, skeletonScreen: boolean) {
     if (capacity < 0) {
       for (var o = 0; o < Math.abs(capacity); o++) {
         var propertyIndex = currentPropertySeatQuantity + o;
-        var thisElement = generateElementOfEventGroup();
-        Field.querySelector('.route_information_calendar_events_groups').appendChild(thisElement.element);
-        var thisDayElement = generateElementOfDay();
-        Field.querySelector('.route_information_calendar_days').appendChild(thisDayElement.element);
+        var thisPropertyElement = generateElementOfProperty();
+        Field.appendChild(thisPropertyElement.element);
       }
     } else {
       for (var o = 0; o < Math.abs(capacity); o++) {
         var propertyIndex = currentPropertySeatQuantity - 1 - o;
-        Field.querySelectorAll(`.route_information_calendar_events_groups .route_information_calendar_grouped_events`)[eventGroupIndex].remove();
-        Field.querySelectorAll(`.route_information_calendar_days .route_information_calendar_day`)[eventGroupIndex].remove();
+        Field.querySelectorAll(`.route_information_property`)[propertyIndex].remove();
       }
     }
   }
