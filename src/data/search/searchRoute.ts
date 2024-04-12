40219a8ce9b7af3d09a4b33d46be7e7802f320ef
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-12 21:46:48 +0800

    Update searchRoute.ts

--- src/data/search/searchRoute.ts
+++ src/data/search/searchRoute.ts
@@ -21,6 +21,24 @@ export async function searchRouteByName(query: string): Array {
   return result;
 }
 
+export async function searchRouteByRouteID(query: string): Array {
+  var requestID = `r_${md5(Math.random() * new Date().getTime())}`;
+  var Route = await getRoute(requestID, true);
+  var result = [];
+  for (var key in Route) {
+    if (String(key).indexOf(query) > -1) {
+      result.push({
+        id: parseInt(key.split('_')[1]),
+        pid: Route[key].pid,
+        dep: Route[key].dep,
+        des: Route[key].des,
+        n: Route[key].n
+      });
+    }
+  }
+  return result;
+}
+
 export async function searchRouteByPathAttributeId(PathAttributeId: [number]) {
   var requestID = `r_${md5(Math.random() * new Date().getTime())}`;
   var Route = await getRoute(requestID, true);
