8ee9c16a491a335b311915eb8b4b3a73bbb55fe0
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-18 16:28:48 +0800

    Update index.ts

---
+++ src/interface/location/index.ts
@@ -0,0 +1,10 @@
+import { integrateLocation } from '../../data/apis/index.ts';
+import { md5 } from 
+
+export function openLocation(hash: string): void {
+
+}
+
+function refreshLocation(): object {
+`r_${md5(Math.random() * new Date().getTime())}`
+}
