177a4223b50545b39809212a4906325f30f1bf94
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-20 08:28:52 +0800

    update

--- src/interface/home-page/index.ts
+++ src/interface/home-page/index.ts
@@ -6,7 +6,7 @@ import { setDataReceivingProgress, getDataReceivingProgress, deleteDataReceiving
 var dataPreloadRequestID = 'preload_data';
 export var dataPreloadCompleted = false;
 var transitioned = false;
-var progressElement = document.querySelector('.home_page_button_right svg#download-svg path[progress="progress"]');
+var progressElement: HTMLElement = document.querySelector('.home_page_button_right svg#download-svg path[progress="progress"]');
 
 function updateDownloadProgress() {
   var pixels = (1 - getDataReceivingProgress(dataPreloadRequestID)) * 189;
@@ -18,6 +18,14 @@ function updateDownloadProgress() {
   });
 }
 
+function setCompleteStatus() {
+  if (dataPreloadCompleted) {
+    document.querySelector('.home_page_button_right').setAttribute('complete', true);
+    progressElement.style.setProperty('--b-stroke-dashoffset', `${0}px`);
+    progressElement.removeEventListener('transitionend', setCompleteStatus);
+  }
+}
+
 export async function preloadData(): void {
   setDataReceivingProgress(dataPreloadRequestID, 'getRoute_0', 0, false);
   setDataReceivingProgress(dataPreloadRequestID, 'getRoute_1', 0, false);
@@ -26,28 +34,10 @@ export async function preloadData(): void {
   setDataReceivingProgress(dataPreloadRequestID, 'getLocation_0', 0, false);
   setDataReceivingProgress(dataPreloadRequestID, 'getLocation_1', 0, false);
   updateDownloadProgress();
-  progressElement.addEventListener(
-    'transitionstart',
-    function () {
-      transitioned = true;
-    },
-    { once: true }
-  );
+  progressElement.addEventListener('transitionend', setCompleteStatus);
   await getRoute(dataPreloadRequestID, true);
   await getStop(dataPreloadRequestID);
   await getLocation(dataPreloadRequestID, true);
   dataPreloadCompleted = true;
-  if (transitioned) {
-    progressElement.addEventListener(
-      'transitionend',
-      function () {
-        document.querySelector('.home_page_button_right').setAttribute('complete', true);
-      },
-      { once: true }
-    );
-  } else {
-    progressElement.style.setProperty('--b-stroke-dashoffset', `${0}px`);
-    document.querySelector('.home_page_button_right').setAttribute('complete', true);
-  }
   deleteDataReceivingProgress(dataPreloadRequestID);
 }
