132dba5ed774de5bbd9e1ae0fe50bfcda11531ac
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-04-28 16:59:38 +0800

    update

--- src/interface/search/index.ts
+++ src/interface/search/index.ts
@@ -7,7 +7,7 @@ import { documentQuerySelector } from '../../tools/query-selector.ts';
 
 const searchPageField = documentQuerySelector('.css_search_field');
 const searchInputElement = documentQuerySelector('.css_search_field .css_search_head .css_search_search_input #search_route_input');
-const searchResultsElement = documentQuerySelector('.css_search_field .css_search_body .css_search_search_results');
+const searchResultsElement = documentQuerySelector('.css_search_field .css_search_body .css_search_results');
 var currentFuse;
 
 export function openSearchPage(): void {
