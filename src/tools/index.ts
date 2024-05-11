1f76fe5a2c3d08d2929e13e049fb6517236b5c67
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-05-11 13:49:58 +0800

    update

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -19,7 +19,7 @@ export function getTextWidth(text: string, weight: number, size: string, fontFam
     var unicode_key: string = `u_${char.charCodeAt(0)}`;
     var charWidth: number = 0;
     if (!cachedTextWidth[configKey].hasOwnProperty(unicode_key)) {
-      charWidth = Math.round(context.measureText(char).width);
+      charWidth = Math.ceil(context.measureText(char).width * 2);
       cachedTextWidth[configKey][unicode_key] = charWidth;
     } else {
       charWidth = cachedTextWidth[configKey][unicode_key];
