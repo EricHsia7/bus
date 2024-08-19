82cbc7e0056cdafd9d69c42d0924752a0a38685a
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-19 19:15:29 +0800

    update

--- src/data/apis/index.ts
+++ src/data/apis/index.ts
@@ -293,9 +293,9 @@ function processEstimateTime(EstimateTime: [], Stop: object, Location: object, B
         var cd = d === 0 ? 0 : (c / d) * 0.5;
         progress = Math.max(0, Math.min(ab + cd, 1));
       }
-      console.log(currentItem._Stop.nameZh, progress);
       currentItem.progress = progress;
     }
+    console.log(currentItem._Stop.nameZh, progress);
     if (multipleEndpoints) {
       if (currentItem._segmentBuffer) {
         endpointCount += 1;
