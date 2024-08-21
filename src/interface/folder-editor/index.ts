9a5bfd7ec9abdb1c1d99a8d69ce4cdd4c90d1544
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-21 12:44:26 +0800

    update

--- src/interface/folder-editor/index.ts
+++ src/interface/folder-editor/index.ts
@@ -1,4 +1,39 @@
-async function initializeFolderEditorField(folderID: string): void {}
+import { Folder, getFolder, listFolderContent } from '../../data/folder/index.ts';
+import { md5 } from '../../tools/index.ts';
+import { documentQuerySelector, elementQuerySelector } from '../../tools/query-selector.ts';
+import { GeneratedElement } from '../index.ts';
+
+function generateElementOfItem(item: object): GeneratedElement {
+  var identifier = `i_${md5(Math.random() + new Date().getTime())}`;
+  var element = document.createElement('div');
+  element.classList.add('css_folder_manager_folder_item');
+  element.id = identifier;
+  element.innerHTML = `${item.type}`;
+  return {
+    element: element,
+    id: identifier
+  };
+}
+
+function updateFolderEditorField(folder: Folder, content: object[]): void {
+  const Field = documentQuerySelector('.css_folder_manager_field');
+  const nameInputElement = elementQuerySelector(Field, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="name"] .css_folder_editor_group_body input');
+  nameInputElement.value = folder.name;
+  const iconSelectElement;
+  const iconInputElement;
+  const folderContentElement = elementQuerySelector(Field, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="content"] .css_folder_editor_group_body');
+  for (var item of content) {
+    var thisItemElement = generateElementOfItem(item);
+    folderContentElement.appendChild(thisItemElement.element);
+  }
+}
+
+async function initializeFolderEditorField(folderID: string): void {
+  //TODO: add skeleton screen
+  var folder = await getFolder(folderID);
+  var content = await listFolderContent(folderID);
+  updateFolderEditorField(folder, content);
+}
 
 export function openFolderEditor(folderID: string): void {
   const Field = documentQuerySelector('.css_folder_manager_field');
