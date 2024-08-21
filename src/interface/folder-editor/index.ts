eeaf35d054f927817bbe59cd56df56bbd769b4b0
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-21 23:41:17 +0800

    update

--- src/interface/folder-editor/index.ts
+++ src/interface/folder-editor/index.ts
@@ -11,7 +11,7 @@ function generateElementOfItem(item: object): GeneratedElement {
   switch (item.type) {
     case 'stop':
       element.classList.add('css_folder_editor_folder_content_stop_item');
-      element.innerHTML = `<div class="css_folder_editor_folder_content_stop_item_route">${item.route ? item.route.name : ''} - 往${item.route ? [item.route.endPoints.destination, item.route.endPoints.departure, ''][item.direction ? item.direction : 0] : ''}</div><div class="css_folder_editor_folder_content_stop_item_name">${item.name}</div><div class="css_folder_editor_folder_content_stop_item_capsule"><div class="css_folder_editor_folder_content_stop_item_sort_control_up">${icons.arrow}</div><div class="css_folder_editor_folder_content_stop_item_sort_control_down">${icons.arrow}</div><div class="css_folder_editor_folder_content_stop_item_delete">${icons.delete}</div></div>`;
+      element.innerHTML = `<div class="css_folder_editor_folder_content_stop_item_route">${item.route ? item.route.name : ''} - 往${item.route ? [item.route.endPoints.destination, item.route.endPoints.departure, ''][item.direction ? item.direction : 0] : ''}</div><div class="css_folder_editor_folder_content_stop_item_name">${item.name}</div><div class="css_folder_editor_folder_content_stop_item_capsule"><div class="css_folder_editor_folder_content_stop_item_sort_control_up">${icons.expand}</div><div class="css_folder_editor_folder_content_stop_item_sort_control_down">${icons.expand}</div><div class="css_folder_editor_folder_content_stop_item_delete">${icons.delete}</div></div>`;
       break;
     default:
       element.innerHTML = '';
