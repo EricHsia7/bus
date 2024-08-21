73c895a369603c47134124ab9a3644d4d6f44158
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-21 08:14:39 +0800

    update

--- src/interface/folder-manager/index.ts
+++ src/interface/folder-manager/index.ts
@@ -1,10 +1,35 @@
-import { documentQuerySelector } from '../../tools/query-selector.ts';
+import { FoldersWithContent, listFoldersWithContent } from '../../data/folder/index.ts';
+import { documentQuerySelector, elementQuerySelector } from '../../tools/query-selector.ts';
+import { icons } from '../icons/index.ts';
+import { GeneratedElement } from '../index.ts';
 
-async function initializeFolderManagerField(): void {}
+function generateElementOfItem(item: FoldersWithContent): GeneratedElement {
+  var identifier = `i_${md5(Math.random() + new Date().getTime())}`;
+  var element = document.createElement('div');
+  element.classList.add('css_folder_manager_folder_item');
+  element.id = identifier;
+  element.innerHTML = `<div class="css_folder_manager_folder_item_icon">${icons.getIconHTML(item.folder.icon)}</div><div class="css_folder_manager_folder_item_name">${item.folder.name}</div><div class="css_folder_manager_folder_item_capsule"></div>`;
+  return {
+    element: element,
+    id: identifier
+  };
+}
+
+async function initializeFolderManagerField(): void {
+  var Field = documentQuerySelector('.css_folder_manager_field');
+  var ListElement = elementQuerySelector(Field, '.css_folder_manager_body .css_folder_manager_folder_list');
+  ListElement.innerHTML = '';
+  var foldersWithContent = await listFoldersWithContent();
+  for (var item of foldersWithContent) {
+    var thisElement = generateElementOfItem(item);
+    ListElement.appendChild(thisElement.element);
+  }
+}
 
 export function openFolderManager(): void {
   var Field = documentQuerySelector('.css_folder_manager_field');
   Field.setAttribute('displayed', 'true');
+  initializeFolderManagerField();
 }
 
 export function closeFolderManager(): void {
