2eec2d97096f67ceed4bcf7c490a31f52a407faa
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-24 15:42:09 +0800

    update

--- src/interface/folder-icon-selector/index.ts
+++ src/interface/folder-icon-selector/index.ts
@@ -7,14 +7,20 @@ import { getIconHTML } from '../icons/index.ts';
 import { GeneratedElement } from '../index.ts';
 import { prompt_message } from '../prompt/index.ts';
 
+type Target = 'editor' | 'creator';
+
 var currentFuse: any = false;
 
-function generateElementOfSymbol(symbol: string): GeneratedElement {
+const folderEditorField = documentQuerySelector('.css_folder_editor_field');
+const folderCreatorField = documentQuerySelector('.css_folder_creator_field');
+const folderIconSelectorField = documentQuerySelector('.css_folder_icon_selector_field');
+
+function generateElementOfSymbol(symbol: string, target: Target): GeneratedElement {
   var identifier = `i_${generateIdentifier()}`;
   var element = document.createElement('div');
   element.id = identifier;
   element.classList.add('css_folder_icon_selector_symbol');
-  element.setAttribute('onclick', `bus.folder.selectFolderIcon('${symbol}')`);
+  element.setAttribute('onclick', `bus.folder.selectFolderIcon('${symbol}', '${target}')`);
   element.innerHTML = getIconHTML(symbol);
   return {
     element: element,
@@ -22,22 +28,20 @@ function generateElementOfSymbol(symbol: string): GeneratedElement {
   };
 }
 
-async function initializeFolderIconSelectorField(): void {
-  const Field = documentQuerySelector('.css_folder_icon_selector_field');
-  const materialSymbolsElement = elementQuerySelector(Field, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols');
+async function initializeFolderIconSelectorField(target: Target): void {
+  const materialSymbolsElement = elementQuerySelector(folderIconSelectorField, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols');
   materialSymbolsElement.innerHTML = '';
   const requestID: string = `r_${generateIdentifier()}`;
   const materialSymbols = await getMaterialSymbols(requestID);
   for (const symbol of materialSymbols) {
-    const symbolElement = generateElementOfSymbol(symbol);
+    const symbolElement = generateElementOfSymbol(symbol, target);
     materialSymbolsElement.appendChild(symbolElement.element);
   }
 }
 
 export function updateMaterialSymbolsSearchResult(query: string): void {
-  const Field = documentQuerySelector('.css_folder_icon_selector_field');
-  const materialSymbolsSearchResultsElement = elementQuerySelector(Field, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols_search_results');
-  const materialSymbolsElement = elementQuerySelector(Field, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols');
+  const materialSymbolsSearchResultsElement = elementQuerySelector(folderIconSelectorField, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols_search_results');
+  const materialSymbolsElement = elementQuerySelector(folderIconSelectorField, '.css_folder_icon_selector_body .css_folder_icon_selector_material_symbols');
   materialSymbolsSearchResultsElement.innerHTML = '';
   if (!containPhoneticSymbols(query) && currentFuse) {
     var searchResults = currentFuse.search(query).slice(0, 30);
@@ -50,21 +54,38 @@ export function updateMaterialSymbolsSearchResult(query: string): void {
   }
 }
 
-export function selectFolderIcon(symbol: string): void {
-  const Field = documentQuerySelector('.css_folder_editor_field');
-  const iconInputElement = elementQuerySelector(Field, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="folder-icon"] .css_folder_editor_group_body .css_folder_editor_icon_input input');
+export function selectFolderIcon(symbol: string, target: Target): void {
+  var iconInputElement;
+  switch (target) {
+    case 'editor':
+      iconInputElement = elementQuerySelector(folderEditorField, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="folder-icon"] .css_folder_editor_group_body .css_folder_editor_icon_input input');
+      break;
+    case 'creator':
+      iconInputElement = elementQuerySelector(folderCreatorField, '.css_folder_creator_body .css_folder_creator_groups .css_folder_creator_group[group="folder-icon"] .css_folder_creator_group_body .css_folder_creator_icon_input input');
+      break;
+    default:
+      break;
+  }
   iconInputElement.value = symbol;
   closeFolderIconSelector();
 }
 
-export function openFolderIconSelector(): void {
-  const folderIconSelectorField = documentQuerySelector('.css_folder_icon_selector_field');
-  const folderEditorField = documentQuerySelector('.css_folder_editor_field');
-  const openFolderIconSelectorElement = elementQuerySelector(folderEditorField, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="folder-icon"] .css_folder_editor_group_body .css_folder_editor_icon_input .css_folder_editor_open_folder_icon_selector');
+export function openFolderIconSelector(target: Target): void {
+  var openFolderIconSelectorElement;
+  switch (target) {
+    case 'editor':
+      openFolderIconSelectorElement = elementQuerySelector(folderEditorField, '.css_folder_editor_body .css_folder_editor_groups .css_folder_editor_group[group="folder-icon"] .css_folder_editor_group_body .css_folder_editor_icon_input .css_folder_editor_open_folder_icon_selector');
+      break;
+    case 'creator':
+      openFolderIconSelectorElement = elementQuerySelector(folderCreatorField, '.css_folder_creator_body .css_folder_creator_groups .css_folder_creator_group[group="folder-icon"] .css_folder_creator_group_body .css_folder_creator_icon_input .css_folder_creator_open_folder_icon_selector');
+      break;
+    default:
+      break;
+  }
   if (openFolderIconSelectorElement.getAttribute('disabled') === 'false') {
     if (dataPreloadCompleted) {
       folderIconSelectorField.setAttribute('displayed', 'true');
-      initializeFolderIconSelectorField();
+      initializeFolderIconSelectorField(target);
       prepareForMaterialSymbolsSearch().then((preparation) => {
         currentFuse = preparation;
       });
@@ -75,6 +96,5 @@ export function openFolderIconSelector(): void {
 }
 
 export function closeFolderIconSelector(): void {
-  const Field = documentQuerySelector('.css_folder_icon_selector_field');
-  Field.setAttribute('displayed', 'false');
+  folderIconSelectorField.setAttribute('displayed', 'false');
 }
