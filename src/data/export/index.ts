30a8635d5ffa36b1e5801cad632b7343f26d37a6
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-25 21:24:32 +0800

    update

--- src/data/export/index.ts
+++ src/data/export/index.ts
@@ -1,8 +1,10 @@
 import { Folder, FolderContent, FoldersWithContent, listFoldersWithContent } from '../folder/index';
 
 export async function exportData(): Promise<string> {
-  let result = {};
   const foldersWithContent = await listFoldersWithContent();
+  let result = {};
+  result.time = new Date().toISOString();
+  result.version = 1;
   result.folders = foldersWithContent;
   return JSON.stringify(result);
 }
