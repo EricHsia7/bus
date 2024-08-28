5e1aff4c0f91ad41b528f0b158325f8464d1ce64
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-29 00:39:33 +0800

    update

--- src/interface/route/index.ts
+++ src/interface/route/index.ts
@@ -103,7 +103,8 @@ export function ResizeRouteCanvas() {
   const FieldSize = queryRouteFieldSize();
   const FieldWidth = FieldSize.width;
   const FieldHeight = FieldSize.height;
-  const canvasScale = Math.round(Math.log10(window.devicePixelRatio) / 0.15904041824);
+  const canvasScale = Math.min(Math.max(Math.round(window.devicePixelRatio / 1.5), 1), 4);
+  //Math.round(Math.log10(window.devicePixelRatio) / 0.15904041824);
   //Math.log(Math.pow(3, 1 / 3) ≈ 0.15904041824
 
   const canvas: HTMLCanvasElement = documentQuerySelector('.css_route_field .css_route_head .css_route_group_tab_line_track .css_route_group_tab_line');
