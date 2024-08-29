8334632e9635166a4e79f56e451c8858b422b40e
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-29 19:37:13 +0800

    update

--- src/interface/search/keyboard.ts
+++ src/interface/search/keyboard.ts
@@ -79,6 +79,6 @@ export function emptyInput(): void {
 }
 
 function uodateCursor(value: string): void {
-  var offset = getTextWidth(value, 500, '20px', `"Noto Sans", sans-serif`, 100, 'normal', 'none', '1.2');
-  documentQuerySelector('#search_style').innerHTML = `:root {--b-cssvar-cursor-offset:${offset}px}`;
+  const offset = getTextWidth(value, 500, '20px', `"Noto Sans", sans-serif`, 100, 'normal', 'none', '1.2');
+  documentQuerySelector('.css_search_field .css_search_head .css_search_search_input .css_cursor').style.setProperty('--b-cssvar-cursor-offset', `${offset}px`);
 }
