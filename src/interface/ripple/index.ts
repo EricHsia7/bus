541713fd0e5ca475ae1c7ce4248a0b1225c9e43a
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-09-16 20:44:14 +0800

    update

--- src/interface/ripple/index.ts
+++ src/interface/ripple/index.ts
@@ -1,9 +1,9 @@
 import { generateIdentifier, supportTouch } from '../../tools/index';
 
-export function addRippleToElement(element: HTMLElement, color: string, duration: number, callback?: Function): void {
+export function addRippleToElement(element: HTMLElement, color: string = 'var(--b-cssvar-333333)', duration: number = 450, callback?: Function): void {
   const eventlistener = supportTouch() ? 'touchstart' : 'mousedown';
   element.addEventListener(eventlistener, (event) => {
-    const ripple_id = generateIdentifier()
+    const rippleID = generateIdentifier();
     const scrollX = document.documentElement.scrollLeft;
     const scrollY = document.documentElement.scrollTop;
     const x = event.pageX;
@@ -22,42 +22,42 @@ export function addRippleToElement(element: HTMLElement, color: string, duration
     const distanceToLeft = relativeX - 0;
     const distanceToRight = elementWidth - relativeX;
     const distanceToBottom = elementHeight - relativeY;
-    const distanceToTopLeftCorner = Math.sqrt(Math.pow(distanceToTop, 2) + Math.pow(distanceToLeft, 2));
-    const distanceToTopRightCorner = Math.sqrt(Math.pow(distanceToTop, 2) + Math.pow(distanceToRight, 2));
-    const distanceToBottomLeftCorner = Math.sqrt(Math.pow(distanceToBottom, 2) + Math.pow(distanceToLeft, 2));
-    const distanceToBottomRightCorner = Math.sqrt(Math.pow(distanceToBottom, 2) + Math.pow(distanceToRight, 2));
+    const distanceToTopLeftCorner = Math.hypot(distanceToTop, distanceToLeft);
+    const distanceToTopRightCorner = Math.hypot(distanceToTop, distanceToRight);
+    const distanceToBottomLeftCorner = Math.hypot(distanceToBottom, distanceToLeft);
+    const distanceToBottomRightCorner = Math.hypot(distanceToBottom, distanceToRight);
     const rippleScale = Math.max(2, Math.max(distanceToTopLeftCorner, distanceToTopRightCorner, distanceToBottomLeftCorner, distanceToBottomRightCorner) / (rippleSize / 2));
 
-    let element_position = getComputedStyle(element).getPropertyValue('position');
-    if (!(element_position === 'absolute') && !(element_position === 'fixed')) {
-      element_position = 'relative';
+    let elementPosition = getComputedStyle(element).getPropertyValue('position');
+    if (!(elementPosition === 'absolute') && !(elementPosition === 'fixed')) {
+      elementPosition = 'relative';
     }
 
-    const css = `.ripple-animation-${ripple_id} {position:${element_position};overflow:hidden;width:${elementWidth}px;height:${elementHeight}px; outline:none; -webkit-tap-highlight-color:rgba(0,0,0,0); -webkit-mask-image: -webkit-radial-gradient(white, black);mask-image: -webkit-radial-gradient(white, black);}.ripple-animation-${ripple_id} .ripple-${ripple_id} {background:${color};width:${rippleSize}px; height:${rippleSize}px;border-radius:50%;position:absolute; top:${rippleBoundaryY}px; left:${rippleBoundaryX}px;transform:scale(0); opacity:0;animation-duration: ${duration}ms;animation-name: ripple-animation-opacity-${ripple_id},ripple-animation-zoom-${ripple_id};animation-iteration-count: forward;animation-timing-function:linear;}@keyframes ripple-animation-opacity-${ripple_id} {0% {opacity:0.15;}60% {opacity:0.15;}100% { opacity:0;} } @keyframes ripple-animation-zoom-${ripple_id} {0% {transform:scale(0.1)}65% {  transform:scale(${rippleScale})}100% {transform:scale(${rippleScale})}}`;
+    const css = `.css_ripple_animation_${rippleID} {position:${elementPosition};overflow:hidden;width:${elementWidth}px;height:${elementHeight}px; outline:none; -webkit-tap-highlight-color:rgba(0,0,0,0); -webkit-mask-image: -webkit-radial-gradient(white, black);mask-image: -webkit-radial-gradient(white, black);}.css_ripple_animation_${rippleID} .css_ripple_${rippleID} {background:${color};width:${rippleSize}px; height:${rippleSize}px; border-radius:50%; position:absolute; top:${rippleBoundaryY}px; left:${rippleBoundaryX}px;transform:scale(0); opacity:0;animation-duration: ${duration}ms;animation-name: css_ripple_animation_opacity-${rippleID},css_ripple_animation_zoom-${rippleID};animation-iteration-count: forward;animation-timing-function:linear;}@keyframes css_ripple_animation_opacity-${rippleID} {0% {opacity:0.15;}60% {opacity:0.15;}100% { opacity:0;} } @keyframes css_ripple_animation_zoom-${rippleID} {0% {transform:scale(0.1)}65% {  transform:scale(${rippleScale})}100% {transform:scale(${rippleScale})}}`;
 
-    element.classList.add(`ripple-animation-${ripple_id}`);
+    element.classList.add(`css_ripple_animation_${rippleID}`);
 
     const styleElement = document.createElement('style');
     styleElement.innerHTML = css;
-    styleElement.id = `ripple-css-${ripple_id}`;
+    styleElement.id = `css_ripple_style_${rippleID}`;
     element.appendChild(styleElement);
 
     const rippleElement = document.createElement('div');
-    rippleElement.id = `ripple-${ripple_id}`;
-    rippleElement.classList.add(`ripple-${ripple_id}`);
+    rippleElement.id = `css_ripple_${rippleID}`;
+    rippleElement.classList.add(`css_ripple_${rippleID}`);
     element.appendChild(rippleElement);
 
     if (typeof callback === 'function') {
-      document.getElementById(`ripple-${ripple_id}`)?.addEventListener(
+      document.getElementById(`css_ripple_${rippleID}`)?.addEventListener(
         'animationstart',
-        () => {
+        function () {
           setTimeout(() => {
             callback!();
 
-            const rippleElement = document.getElementById(`ripple-${ripple_id}`);
-            const cssStyleElement = document.getElementById(`ripple-css-${ripple_id}`);
+            const rippleElement = document.getElementById(`css_ripple_${rippleID}`);
+            const cssStyleElement = document.getElementById(`css_ripple_style_${rippleID}`);
             if (rippleElement) {
-              element.classList.remove(`ripple-animation-${ripple_id}`);
+              element.classList.remove(`css_ripple_animation_${rippleID}`);
               rippleElement.remove();
             }
             if (cssStyleElement) {
@@ -68,13 +68,13 @@ export function addRippleToElement(element: HTMLElement, color: string, duration
         { once: true }
       );
     } else {
-      document.getElementById(`ripple-${ripple_id}`)?.addEventListener(
+      document.getElementById(`css_ripple_${rippleID}`)?.addEventListener(
         'animationend',
-        () => {
-          const rippleElement = document.getElementById(`ripple-${ripple_id}`);
-          const cssStyleElement = document.getElementById(`ripple-css-${ripple_id}`);
+        function () {
+          const rippleElement = document.getElementById(`css_ripple_${rippleID}`);
+          const cssStyleElement = document.getElementById(`css_ripple_style_${rippleID}`);
           if (rippleElement) {
-            element.classList.remove(`ripple-animation-${ripple_id}`);
+            element.classList.remove(`css_ripple_animation_${rippleID}`);
             rippleElement.remove();
           }
           if (cssStyleElement) {
@@ -83,11 +83,11 @@ export function addRippleToElement(element: HTMLElement, color: string, duration
         },
         { once: true }
       );
-      setTimeout(() => {
-        const rippleElement = document.getElementById(`ripple-${ripple_id}`);
-        const cssStyleElement = document.getElementById(`ripple-css-${ripple_id}`);
+      setTimeout(function () {
+        const rippleElement = document.getElementById(`css_ripple_${rippleID}`);
+        const cssStyleElement = document.getElementById(`css_ripple_style_${rippleID}`);
         if (rippleElement) {
-          element.classList.remove(`ripple-animation-${ripple_id}`);
+          element.classList.remove(`css_ripple_animation_${rippleID}`);
           rippleElement.remove();
         }
         if (cssStyleElement) {
