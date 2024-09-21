afc3be43e9c87e2e9dd1da809d46efa2586097eb
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-09-21 14:51:24 +0800

    update

--- cloudflare/bus-proxy.js
+++ cloudflare/bus-proxy.js
@@ -1,26 +1,36 @@
 export default {
   async fetch(request, env, ctx) {
-    // Extract the URL of the request made to the worker
-    const url = new URL(request.url);
+    const allowedDomain = 'https://erichsia7.github.io';
 
-    // Get query parameters from the original request
-    const params = new URLSearchParams(url.search);
+    // Get the 'Referer' header from the incoming request
+    const referer = request.headers.get('referer');
 
-    // Extract the query parameters
-    const apiUrl = params.get('url');
-    const _ = params.get('_');
+    // Check if the 'Referer' matches the allowed domain
+    if (referer && referer.startsWith(allowedDomain)) {
+      // Extract the URL of the request made to the worker
+      const url = new URL(request.url);
 
-    // Forward the modified request to the API
-    const response = await fetch(`${apiUrl}?_=${_}`, {
-      method: request.method,
-      headers: request.headers,
-      body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.text() : null
-    });
+      // Get query parameters from the original request
+      const params = new URLSearchParams(url.search);
 
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers
-    });
+      // Extract the query parameters
+      const apiUrl = params.get('url');
+      const _ = params.get('_');
+
+      // Forward the modified request to the API
+      const response = await fetch(`${apiUrl}?_=${_}`, {
+        method: request.method,
+        headers: request.headers,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.text() : null
+      });
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers
+      });
+    } else {
+      return new Response('access denied');
+    }
   }
 };
