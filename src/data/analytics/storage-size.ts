7d81abe2e7e97ea30920634b5cd98d324e966018
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-09-22 19:23:42 +0800

    update

--- src/data/analytics/storage-size.ts
+++ src/data/analytics/storage-size.ts
@@ -1,68 +1,116 @@
 import { convertBytes } from '../../tools/index';
 import { getStoreKey, getStoresLength, lfGetItem, lfListItemKeys } from '../storage/index';
 
-function storeIndexToCategory(store: number): string {
+interface StoreCategory {
+  name: string;
+  key: string;
+}
+
+interface StoreSizeInBytes {
+  category: StoreCategory;
+  size: number;
+}
+
+type CategorizedSizesInBytes = { [key: string]: StoreSizeInBytes };
+
+interface StoreSize {
+  category: StoreCategory;
+  size: number;
+}
+
+type CategorizedSizes = { [key: string]: StoreSize };
+
+function storeIndexToCategory(store: number): StoreCategory {
   const storeKey = getStoreKey(store);
-  let result = '';
+  let name = '';
+  let key = '';
   switch (storeKey) {
     case 'cacheStore':
-      result = '快取';
+      name = '快取';
+      key = 'cache';
       break;
     case 'settingsStore':
-      result = '設定';
+      name = '設定';
+      key = 'settings';
       break;
     case 'dataUsageRecordsStore':
-      result = '分析';
+      name = '分析';
+      key = 'analytics';
       break;
     case 'updateRateRecordsStore':
-      result = '分析';
+      name = '分析';
+      key = 'analytics';
       break;
     case 'folderListStore':
-      result = '資料夾';
+      name = '資料夾';
+      key = 'folders';
       break;
     case 'savedStopFolderStore':
-      result = '資料夾';
+      name = '資料夾';
+      key = 'folders';
       break;
     case 'savedRouteFolderStore':
-      result = '資料夾';
+      name = '資料夾';
+      key = 'folders';
       break;
     default:
       if (storeKey.startsWith('F') && storeKey.endsWith('Store')) {
-        result = '資料夾';
+        name = '資料夾';
+        key = 'folders';
       } else {
-        result = '其他';
+        name = '其他';
+        key = 'others';
       }
       break;
   }
+  const result = {
+    name,
+    key
+  };
   return result;
 }
 
 export async function getStoresSizeStatistics(): Promise<object> {
-  let totalSizeInByte = 0;
-  let categorizedSizes: { [key: string]: string } = {};
+  let totalSizeInBytes = 0;
+  let categorizedSizesInBytes: CategorizedSizesInBytes = {};
   const storesLength = getStoresLength();
 
   for (let i = 0; i < storesLength; i++) {
     const keysInStore = await lfListItemKeys(i);
-    let thisCategorySize = 0;
+    let thisStoreSizeInBytes = 0;
     for (const itemKey of keysInStore) {
       const item = await lfGetItem(i, itemKey);
       const itemInString = String(item);
       const itemLength = itemInString.length + itemKey.length;
-      totalSizeInByte += itemLength;
-      thisCategorySize += itemLength;
+      totalSizeInBytes += itemLength;
+      thisStoreSizeInBytes += itemLength;
     }
-    const categoryKey = `c_${i}`;
-    categorizedSizes[categoryKey] = {
-      category: storeIndexToCategory(i),
+    const thisCategory = storeIndexToCategory(i);
+    const thisCategoryKey = thisCategory.key;
+    if (!categorizedSizesInBytes.hasOwnProperty(thisCategoryKey)) {
+      categorizedSizesInBytes[thisCategoryKey] = {
+        category: thisCategory,
+        size: 0
+      };
+    }
+    categorizedSizesInBytes[thisCategory.key].size = categorizedSizesInBytes[thisCategory.key].size + thisStoreSizeInBytes;
+  }
+
+  const totalSize = convertBytes(totalSizeInBytes);
+
+  let categorizedSizes: CategorizedSizes = {};
+  for (const key in categorizedSizesInBytes) {
+    const thisCategory = categorizedSizesInBytes[key].category;
+    const thisCategorySize = categorizedSizesInBytes[key].size;
+    categorizedSizes[key] = {
+      category: thisCategory,
       size: convertBytes(thisCategorySize)
     };
   }
-  const totalSize = convertBytes(totalSizeInByte);
 
   const result = {
     totalSize,
-    categorizedSizes
+    categorizedSizes: categorizedSizes
   };
 
   return result;
