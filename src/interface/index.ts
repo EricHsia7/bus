b1ee1fe84f081a456bcc41333b7874b31c41cef2
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-09-26 23:43:42 +0800

    update

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -11,7 +11,7 @@ const splashScreenTimer = {
   openTime: new Date().getTime()
 };
 
-type Page = 'Home' | 'FolderCreator' | 'FolderEditor' | 'FolderIconSelector' | 'FolderManager' | 'Location' | 'RouteDetails' | 'Route' | 'SaveToFolder' | 'Search' | 'Settings' | 'SettingsOptions' | 'DataUsage' | 'PersonalScheduleManager';
+type Page = 'Home' | 'FolderCreator' | 'FolderEditor' | 'FolderIconSelector' | 'FolderManager' | 'Location' | 'RouteDetails' | 'Route' | 'SaveToFolder' | 'Search' | 'Settings' | 'SettingsOptions' | 'DataUsage' | 'PersonalScheduleManager' | 'PersonalScheduleCreator';
 
 let pageHistory: Array<Page> = ['Home'];
 
--- src/interface/personal-schedule-creator/index.ts
+++ src/interface/personal-schedule-creator/index.ts
@@ -1,11 +1,15 @@
 import { documentQuerySelector } from '../../tools/query-selector';
+import { openPreviousPage, pushPageHistory } from '../index';
 
 const PersonalScheduleCreatorField = documentQuerySelector('.css_personal_schedule_creator_field');
 
 export function openPersonalScheduleCreator(): void {
+  pushPageHistory('PersonalScheduleCreator');
   PersonalScheduleCreatorField.setAttribute('displayed', 'true');
 }
 
 export function closePersonalScheduleCreator(): void {
+  // revokePageHistory('PersonalScheduleCreator');
   PersonalScheduleCreatorField.setAttribute('displayed', 'false');
+  openPreviousPage();
 }
