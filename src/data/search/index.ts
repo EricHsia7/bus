4b1124374400ec46ca6d8c59e16cb7a171b2aa6a
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-05 18:39:42 +0800

    update

--- src/data/search/index.ts
+++ src/data/search/index.ts
@@ -1,8 +1,12 @@
 import { getRoute, SimplifiedRouteItem } from '../apis/getRoute/index';
 import { getLocation } from '../apis/getLocation/index';
-import { generateIdentifier } from '../../tools/index';
+import { generateIdentifier, getIntersection, getUnicodes } from '../../tools/index';
 
-const Fuse = require('fuse.js/basic');
+// const Fuse = require('fuse.js/basic');
+
+let searchIndex = {};
+let searchList = [];
+let readyToSearch = false;
 
 export async function searchRouteByName(query: string): Promise<Array> {
   var requestID = generateIdentifier('r');
@@ -59,12 +63,15 @@ export async function searchRouteByPathAttributeId(PathAttributeId: Array<number
 }
 
 export async function prepareForSearch(): void {
+  readyToSearch = false;
   const requestID = generateIdentifier('r');
   const Route = await getRoute(requestID, true);
   const mergedLocation = await getLocation(requestID, true);
+  let index = {};
   let list = [];
+  let i = 0;
   for (const key in Route) {
-    list.push({
+    const thisItem = {
       id: parseInt(key.split('_')[1]),
       pid: Route[key].pid,
       dep: Route[key].dep,
@@ -76,10 +83,20 @@ export async function prepareForSearch(): void {
       r: '',
       s: '',
       type: 0
-    });
+    };
+    list.push(thisItem);
+    const unicodes = getUnicodes(thisItem.n).concat(getUnicodes(thisItem.dep)).concat(getUnicodes(thisItem.des));
+    for (const unicode of unicodes) {
+      const key = `u_${unicode}`;
+      if (!index.hasOwnProperty(key)) {
+        index[key] = [];
+      }
+      index[key].push(i);
+    }
+    i += 1;
   }
   for (const key in mergedLocation) {
-    list.push({
+    const thisItem = {
       id: mergedLocation[key].id,
       n: mergedLocation[key].n,
       lo: mergedLocation[key].lo,
@@ -91,15 +108,53 @@ export async function prepareForSearch(): void {
       des: '',
       pid: [],
       type: 1
-    });
+    };
+    list.push(thisItem);
+    const unicodes = getUnicodes(thisItem.n);
+    for (const unicode of unicodes) {
+      const key = `u_${unicode}`;
+      if (!index.hasOwnProperty(key)) {
+        index[key] = [];
+      }
+      index[key].push(i);
+    }
+    i += 1;
   }
-  console.log(list);
-  console.log(list.length);
-  return new Fuse(list, {
-    keys: [
-      { name: 'n', weight: 0.7 },
-      { name: 'dep', weight: 0.15 },
-      { name: 'des', weight: 0.15 }
-    ]
+  searchIndex = index;
+  searchList = list;
+  readyToSearch = true;
+}
+
+export function searchFor(query: string): Array {
+  if (!readyToSearch) {
+    return [];
+  }
+  const unicodes = getUnicodes(query);
+  let unicodeGroups = [];
+  for (const unicode of unicodes) {
+    const key = `u_${unicode}`;
+    if (searchIndex.hasOwnProperty(key)) {
+      unicodeGroups.push(searchIndex[key]);
+    }
+  }
+  unicodeGroups.sort(function (a, b) {
+    return a.length - b.length;
   });
+  const unicodeGroupsLength1 = unicodeGroups.length - 1;
+  let intersection = [];
+  for (let i = 0; i < unicodeGroupsLength1; i++) {
+    const currentGroup = unicodeGroups[i];
+    const nextGroup = unicodeGroups[i + 1];
+    if (i === 0) {
+      intersection = getIntersection(currentGroup, nextGroup);
+    } else {
+      intersection = getIntersection(intersection, nextGroup);
+    }
+  }
+  let result = [];
+  for (const j of intersection) {
+    const thisItem = searchList[j];
+    result.push(thisItem);
+  }
+  return result;
 }
--- src/interface/search/index.ts
+++ src/interface/search/index.ts
@@ -1,5 +1,5 @@
 import { openKeyboard, closeKeyboard } from './keyboard';
-import { prepareForSearch } from '../../data/search/index';
+import { prepareForSearch, searchFor } from '../../data/search/index';
 import { getIconHTML } from '../icons/index';
 import { dataDownloadCompleted } from '../home/index';
 import { promptMessage } from '../prompt/index';
@@ -10,16 +10,13 @@ import { pushPageHistory, revokePageHistory } from '../index';
 const searchField = documentQuerySelector('.css_search_field');
 const searchInputElement = documentQuerySelector('.css_search_field .css_search_head .css_search_search_input #search_input');
 const searchResultsElement = documentQuerySelector('.css_search_field .css_search_body .css_search_results');
-var currentFuse: any = false;
 
 export function openSearch(): void {
   if (dataDownloadCompleted) {
     pushPageHistory('Search');
     searchField.setAttribute('displayed', 'true');
     openKeyboard();
-    prepareForSearch().then((preparation) => {
-      currentFuse = preparation;
-    });
+    prepareForSearch();
   } else {
     promptMessage('資料還在下載中', 'download_2');
   }
@@ -32,19 +29,23 @@ export function closeSearch(): void {
 }
 
 export function updateSearchResult(query: string): void {
-  if (!containPhoneticSymbols(query) && currentFuse) {
-    var typeToIcon = ['route', 'location_on'];
-    var searchResults = currentFuse.search(query).slice(0, 30);
-    var html = [];
-    for (var result of searchResults) {
-      var name = result.item.n;
-      var typeIcon = getIconHTML(typeToIcon[result.item.type]);
-      var onclickScript = '';
-      if (result.item.type === 0) {
-        onclickScript = `bus.route.openRoute(${result.item.id}, [${result.item.pid.join(',')}])`;
-      }
-      if (result.item.type === 1) {
-        onclickScript = `bus.location.openLocation('${result.item.hash}')`; //openLocation
+  if (!containPhoneticSymbols(query)) {
+    const typeToIcon = ['route', 'location_on'];
+    const searchResults = searchFor(query).slice(0, 30);
+    let html = [];
+    for (const result of searchResults) {
+      const name = result.item.n;
+      const typeIcon = getIconHTML(typeToIcon[result.item.type]);
+      let onclickScript = '';
+      switch (result.item.type) {
+        case 0:
+          onclickScript = `bus.route.openRoute(${result.item.id}, [${result.item.pid.join(',')}])`;
+          break;
+        case 1:
+          onclickScript = `bus.location.openLocation('${result.item.hash}')`;
+          break;
+        default:
+          break;
       }
       html.push(`<div class="css_search_search_result" onclick="${onclickScript}"><div class="css_search_search_result_type">${typeIcon}</div><div class="css_search_search_result_route_name">${name}</div></div>`);
     }
