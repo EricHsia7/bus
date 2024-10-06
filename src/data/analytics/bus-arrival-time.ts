9cfd51bfc10616e36bdad6b2fff214b2d24a1466
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-06 17:52:42 +0800

    update

--- src/data/analytics/bus-arrival-time.ts
+++ src/data/analytics/bus-arrival-time.ts
@@ -8,7 +8,7 @@ import { lfGetItem, lfListItemKeys, lfSetItem } from '../storage/index';
 let trackingBusArrivalTime_trackingID: string = '';
 let trackingBusArrivalTime_tracking: boolean = false;
 let trackingBusArrivalTime_trackedStops: Array = [];
-const trackingBusArrivalTime_monitorTimes: number = 128;
+const trackingBusArrivalTime_monitorTimes: number = 64;
 
 interface EstimateTimeRecordForBusArrivalTime {
   EstimateTime: number;
