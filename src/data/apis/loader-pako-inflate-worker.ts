424afe072d4d2c0a0589d5aa9e54cf699d9eb01b
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-08 08:06:20 +0800

    update

--- src/data/apis/loader-pako-inflate-worker.ts
+++ src/data/apis/loader-pako-inflate-worker.ts
@@ -4,27 +4,13 @@ let taskQueue = []; // Queue to store tasks
 let processing = false; // Flag to check if a task is being processed
 
 self.onmessage = function (e) {
-  taskQueue.push(e.data);
-  processNextTask(); // Try to process the next task
+  const task = e.data;
+  const [buffer, taskID] = task;
+  const result = pakoInflate_worker(buffer);
+  this.self.postMessage([result, taskID]);
 };
 
 function pakoInflate_worker(buffer: ArrayBuffer): string {
   const inflatedData = inflate(buffer, { to: 'string' }); // Inflate and convert to string using pako
   return inflatedData;
-}
-
-function processNextTask() {
-  if (processing || taskQueue.length === 0) {
-    return; // If already processing or no tasks, exit
-  }
-
-  processing = true; // Set the flag to processing
-  const task = taskQueue.shift();
-  const buffer = task[0]; // Get the next task
-  const taskID = task[1];
-  const result = pakoInflate_worker(buffer);
-  self.postMessage([result, taskID]); // Send the result back to the main thread
-
-  processing = false; // Reset processing flag
-  processNextTask(); // Process the next task in the queue
 }
