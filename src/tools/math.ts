07a9529c6a38f323b9b3e8f143260ce87950f16c
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-11 23:11:53 +0800

    update

--- src/tools/math.ts
+++ src/tools/math.ts
@@ -99,34 +99,51 @@ export function calculateAverage(array: Array<number>): number {
   }
 }
 
-export function aggregateNumbers(array: Array<number>): Array<number> {
-  let roundedArray = [];
-  for (const item of array) {
-    let roundedItem = Math.trunc(item);
-    if (!roundedArray.includes(roundedItem)) {
-      roundedArray.push(roundedItem);
-    }
+export function aggregateNumbers(array: Array<number>, exponent: number): Array<number> {
+  const arrLength = array.length;
+
+  if (arrLength < 3) {
+    return array;
   }
 
+  let sum: number = 0;
+  for (let i = 0; i < arrLength; i++) {
+    sum += array[i];
+  }
+  let average = sum / arrLength;
+
+  let SquaredDeviationSum: number = 0;
+  for (let j = 0; j < arrLength; j++) {
+    SquaredDeviationSum += Math.pow(array[j] - average, 2);
+  }
+  const standardDeviation = Math.sqrt(SquaredDeviationSum / arrLength);
+
+  let exponentials = [];
+  let exponentialSum = 0;
+  for (let k = 0; k < arrLength; k++) {
+    const exponential = Math.exp(array[k] / standardDeviation);
+    exponentialSum += exponential;
+    exponentials.push(exponential);
+  }
+
+  let P = Math.pow(arrLength, exponent);
   let groupedNumbers = {};
-  const base = Math.log(1.2);
-  for (const item2 of roundedArray) {
-    let groupKey = `k_${Math.round(Math.log(Math.max(item2, 1)) / base)}`;
-    if (!groupedNumbers.hasOwnProperty(groupKey)) {
-      groupedNumbers[groupKey] = [];
+  for (let l = 0; l < arrLength; l++) {
+    const key = `k_${Math.floor((exponentials[l] / exponentialSum) * P)}`;
+    if (!groupedNumbers.hasOwnProperty(key)) {
+      groupedNumbers[key] = {
+        sum: 0,
+        len: 0
+      };
     }
-    groupedNumbers[groupKey].push(item2);
+    groupedNumbers[key].sum += array[l];
+    groupedNumbers[key].len += 1;
   }
 
   let result = [];
   for (const key in groupedNumbers) {
-    let group = groupedNumbers[key];
-    const groupLength = group.length;
-    let average = 0;
-    if (groupLength > 0) {
-      average = group.reduce((a, b) => a + b, 0) / groupLength;
-    }
-    result.push(Math.trunc(average));
+    const thisGroup = groupedNumbers[key];
+    result.push(Math.floor(thisGroup.sum / thisGroup.len));
   }
 
   result.sort((a, b) => a - b);
