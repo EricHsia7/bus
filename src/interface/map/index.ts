0092d91aec1457aecd75079eebfa540c0f61c8d1
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-15 21:50:44 +0800

    update

--- src/interface/map/index.ts
+++ src/interface/map/index.ts
@@ -16,7 +16,7 @@ let canvasHeight = window.innerHeight * devicePixelRatio;
 
 const chunkWidth = 300;
 const chunkHeight = 300;
-const resolution = 300;
+const resolution = 100000;
 
 const lineWidth = 5;
 const pointRadius = 3;
