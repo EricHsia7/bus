8a38eb6b88d049cac6fcad16b7d2427279caa660
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-17 07:48:35 +0800

    Update convert.ts

--- src/tools/convert.ts
+++ src/tools/convert.ts
@@ -1,11 +1,30 @@
+const ErathRadius = 6378137;
+
 export function convertPositionsToDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-  const R = 6378.137;
   const dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
   const dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
   const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const d = R * c;
-  return d * 1000; // measured in meters
+  const d = ErathRadius * c;
+  return d; // measured in meters
+}
+
+export function mercatorProjection(lat: number, lon: number, scale = 1) {
+    const meterToPixelRatio = 100 / 100; // 100 meters = 100 pixels
+    
+    // Convert degrees to radians
+    const latRad = (lat * Math.PI) / 180;
+    const lonRad = (lon * Math.PI) / 180;
+
+    // Mercator projection formulas
+    const x = ErathRadius * lonRad;
+    const y = ErathRadius * Math.log(Math.tan(Math.PI / 4 + latRad / 2));
+
+    // Scale based on the ratio (meters to pixels)
+    const xPixels = x * meterToPixelRatio * scale;
+    const yPixels = y * meterToPixelRatio * scale;
+
+    return { x: xPixels, y: yPixels };
 }
 
 export function convertBytes(contentLength: number): string {
