460d0542d2776be12c864566023b8b738e9c7012
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-01-28 01:49:32 +0800

    update

--- src/interface/notification-schedule-manager/index.ts
+++ src/interface/notification-schedule-manager/index.ts
@@ -1,11 +1,16 @@
-import { documentQuerySelector } from "../../tools/query-selector";
+import { documentQuerySelector } from '../../tools/query-selector';
+import { closePreviousPage, openPreviousPage, pushPageHistory, revokePageHistory } from '../index';
 
-const NotificationScheduleManagerField = documentQuerySelector('.')
+const NotificationScheduleManagerField = documentQuerySelector('.css_notification_schedule_manager_field');
 
 export function openNotificationScheduleManager(): void {
-  
+  pushPageHistory('NotificationScheduleManager');
+  NotificationScheduleManagerField.setAttribute('displayed', 'true');
+  closePreviousPage();
 }
 
 export function closeNotificationScheduleManager(): void {
-
+  // revokePageHistory('NotificationScheduleManager');
+  NotificationScheduleManagerField.setAttribute('displayed', 'false');
+  openPreviousPage();
 }
