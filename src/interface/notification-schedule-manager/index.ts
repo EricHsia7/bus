7459aaeec20e83d71dd6be2820b05693ad41ea70
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-01-28 14:29:36 +0800

    update

--- src/interface/notification-schedule-manager/index.ts
+++ src/interface/notification-schedule-manager/index.ts
@@ -1,7 +1,18 @@
-import { documentQuerySelector } from '../../tools/query-selector';
-import { closePreviousPage, openPreviousPage, pushPageHistory, revokePageHistory } from '../index';
+import { listNotifcationSchedules, NScheduleFrontend } from '../../data/notification/index';
+import { documentQuerySelector, elementQuerySelector } from '../../tools/query-selector';
+import { closePreviousPage, openPreviousPage, pushPageHistory } from '../index';
 
 const NotificationScheduleManagerField = documentQuerySelector('.css_notification_schedule_manager_field');
+const NotificationScheduleManagerBody = elementQuerySelector(NotificationScheduleManagerField, '.css_notification_schedule_manager_body');
+
+function updateNotificationScheduleManagerField(notificationSchedules: Array<NScheduleFrontend>): void {
+  
+}
+
+function initializeNotificationScheduleManagerField(): void {
+  const notificationSchedules = listNotifcationSchedules();
+  updateNotificationScheduleManagerField(notificationSchedules);
+}
 
 export function openNotificationScheduleManager(): void {
   pushPageHistory('NotificationScheduleManager');
