716899e9bd41d2b9577447b3e3e1030c512102bd
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-02-04 16:45:34 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,23 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'no-const-assign': 'warn', // Prevents reassignment of `const` variables
-    '@typescript-eslint/no-redeclare': 'warn', // Prevents redeclaring variables
-    'prefer-const': 'warn',
-    'prefer-const': 'warn',
-    'no-var': 'warn',
 
     // Object Methods
     'no-prototype-builtins': 'warn',
 
     // Code Style
+    'prefer-const': 'warn',
+    'no-var': 'warn',
     'no-case-declarations': 'warn',
     'no-useless-escape': 'warn',
+    'no-constant-binary-expression': 'warn',
 
     // TypeScript Specific
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-require-imports': 'warn',
-    '@typescript-eslint/no-redeclare': 'warn',
     '@typescript-eslint/no-empty-object-type': 'warn',
+    '@typescript-eslint/no-redeclare': 'warn',
 
     // Functions
     'no-async-promise-executor': 'warn',
