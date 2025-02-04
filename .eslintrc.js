ee85f50c0b0baecbc6eebbfdf3485c158e7d21f3
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-02-04 16:30:26 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    'no-const-assign': 'warning', // Prevents reassignment of `const` variables
-    '@typescript-eslint/no-redeclare': 'warning' // Prevents redeclaring variables
+    'no-const-assign': 'warn', // Prevents reassignment of `const` variables
+    '@typescript-eslint/no-redeclare': 'warn' // Prevents redeclaring variables
   }
 };
