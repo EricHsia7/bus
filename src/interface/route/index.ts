5ca1470cfe49a07810114e40a416fc703e8235ed
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-03-16 19:34:25 +0800

    update

--- src/interface/route/index.ts
+++ src/interface/route/index.ts
@@ -718,7 +718,7 @@ export function stretchRouteItem(itemElementID: string, threadBoxElementID: stri
     for (let i = 0; i < elementsBelowLength; i++) {
       const thisItemElement = elementsBelowItemElement[i];
       const thisThreadBoxElement = elementsBelowThreadBoxElement[i];
-      thisThreadBoxElement.style.setProperty('--b-cssvar-css-route-group-thread-z-index', (elementsBelowLength - i).toString());
+      thisThreadBoxElement.style.setProperty('--b-cssvar-css-route-group-thread-z-index', (elementsBelowLength - i - 1).toString());
       thisThreadBoxElement.setAttribute('push-direction', pushDirection);
       thisThreadBoxElement.setAttribute('push-state', '1');
       thisItemElement.setAttribute('push-direction', pushDirection);
