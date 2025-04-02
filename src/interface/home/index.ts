68a82f2ea0fd834b7cbe098d7b2904f86bb5146a
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-04-02 20:36:06 +0800

    update

--- src/interface/home/index.ts
+++ src/interface/home/index.ts
@@ -1,6 +1,6 @@
 import { getRoute } from '../../data/apis/getRoute/index';
 import { getLocation } from '../../data/apis/getLocation/index';
-import { setDataReceivingProgress, getDataReceivingProgress, deleteDataReceivingProgress, deleteDataUpdateTime } from '../../data/apis/loader';
+import { setDataReceivingProgress, getDataReceivingProgress, deleteDataReceivingProgress, deleteDataUpdateTime, DataReceivingProgressEvent } from '../../data/apis/loader';
 import { documentQuerySelector } from '../../tools/query-selector';
 import { getMaterialSymbols } from '../../data/apis/getMaterialSymbols/index';
 import { getCarInfo } from '../../data/apis/getCarInfo/index';
@@ -9,21 +9,15 @@ const dataDownloadRequestID = 'downloadData';
 export let dataDownloadCompleted = false;
 const progressElement: HTMLElement = documentQuerySelector('.css_home_button_right svg#download-svg path[progress="progress"]');
 
-function updateDownloadProgress() {
-  var pixels = (1 - getDataReceivingProgress(dataDownloadRequestID)) * 189;
+function handleDataReceivingProgressUpdates(event: Event): void {
+  const CustomEvent = event as DataReceivingProgressEvent;
+  const pixels = (1 - getDataReceivingProgress(dataDownloadRequestID)) * 189;
   progressElement.style.setProperty('--b-cssvar-stroke-dashoffset', `${pixels}px`);
-  window.requestAnimationFrame(function () {
-    if (dataDownloadCompleted === false) {
-      updateDownloadProgress();
-    }
-  });
-}
-
-function setCompleteStatus() {
-  if (dataDownloadCompleted) {
+  if (CustomEvent.detail.stage === 'end') {
+    document.removeEventListener(CustomEvent.detail.target, handleDataReceivingProgressUpdates);
     progressElement.style.setProperty('--b-cssvar-stroke-dashoffset', `${0}px`);
     documentQuerySelector('.css_home_button_right').setAttribute('complete', 'true');
-    progressElement.removeEventListener('transitioncancel', setCompleteStatus);
+    dataDownloadCompleted = true;
   }
 }
 
@@ -38,14 +32,11 @@ export async function downloadData() {
   setDataReceivingProgress(dataDownloadRequestID, 'getCarInfo_0', 0, false);
   setDataReceivingProgress(dataDownloadRequestID, 'getCarInfo_1', 0, false);
   setDataReceivingProgress(dataDownloadRequestID, 'getMaterialSymbols', 0, false);
-  updateDownloadProgress();
-  progressElement.addEventListener('transitioncancel', setCompleteStatus);
+  document.addEventListener(dataDownloadRequestID, handleDataReceivingProgressUpdates);
   await getRoute(dataDownloadRequestID, true);
   await getLocation(dataDownloadRequestID, 1);
   await getCarInfo(dataDownloadRequestID, true);
   await getMaterialSymbols(dataDownloadRequestID);
-  dataDownloadCompleted = true;
-  setCompleteStatus();
   deleteDataReceivingProgress(dataDownloadRequestID);
   deleteDataUpdateTime(dataDownloadRequestID);
 }
