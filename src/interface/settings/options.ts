4040b943dc0c52d6c66242e705635d090af4537b
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-04-06 14:42:32 +0800

    update

--- src/interface/settings/options.ts
+++ src/interface/settings/options.ts
@@ -1,57 +1,59 @@
 import { changeSettingOption, getSetting } from '../../data/settings/index';
-import { generateIdentifier } from '../../tools/index';
 import { documentQuerySelector, documentQuerySelectorAll, elementQuerySelector } from '../../tools/query-selector';
 import { closePreviousPage, GeneratedElement, openPreviousPage, pushPageHistory } from '../index';
 
+const SettingsOptionsField = documentQuerySelector('.css_settings_options_field');
+const SettingsOptionsBodyElement = elementQuerySelector(SettingsOptionsField, '.css_settings_options_body');
+const optionsElement = elementQuerySelector(SettingsOptionsBodyElement, '.css_settings_options');
+const descriptionElement = elementQuerySelector(SettingsOptionsBodyElement, '.css_options_description');
+const SettingsOptionsHeadElement = elementQuerySelector(SettingsOptionsField, '.css_settings_options_head');
+const titleElement = elementQuerySelector(SettingsOptionsHeadElement, '.css_settings_options_title');
+
 function generateElementOfItem(setting: object, item: object, index: number): GeneratedElement {
-  var identifier = generateIdentifier('i');
-  var element = document.createElement('div');
+  // const identifier = generateIdentifier('i');
+  const element = document.createElement('div');
   element.classList.add('css_option');
-  element.id = identifier;
+  // element.id = identifier;
   element.innerHTML = /*html*/ `<div class="css_option_name">${item.name}</div><div class="css_option_checkbox"><input type="checkbox" onclick="bus.settings.settingsOptionsHandler(event, '${setting.key}', ${index})" ${setting.option === index ? 'checked' : ''}/></div>`;
   return {
     element: element,
-    id: identifier
+    id: ''
   };
 }
 
-function initializeSettingsOptionsField(Field: HTMLElement, settingKey: string): void {
+function initializeSettingsOptionsField(settingKey: string): void {
   const setting = getSetting(settingKey);
-  const bodyElement = elementQuerySelector(Field, '.css_settings_options_body');
-  const optionsElement = elementQuerySelector(bodyElement, '.css_settings_options');
-  const descriptionElement = elementQuerySelector(bodyElement, '.css_options_description');
+  titleElement.innerText = setting.name;
   descriptionElement.innerText = setting.description;
   optionsElement.innerHTML = '';
+  const fragment = new DocumentFragment();
   let index = 0;
   for (const item of setting.options) {
     const thisElement = generateElementOfItem(setting, item, index);
-    optionsElement.appendChild(thisElement.element);
+    fragment.appendChild(thisElement.element);
     index += 1;
   }
+  optionsElement.append(fragment);
 }
 
 export function openSettingsOptions(settingKey: string): void {
   pushPageHistory('SettingsOptions');
-  var setting = getSetting(settingKey);
-  var Field: HTMLElement = documentQuerySelector('.css_settings_options_field');
-  Field.setAttribute('displayed', 'true');
-  elementQuerySelector(Field, '.css_settings_options_head .css_settings_options_title').innerText = setting.name;
-  initializeSettingsOptionsField(Field, settingKey);
+  SettingsOptionsField.setAttribute('displayed', 'true');
+  initializeSettingsOptionsField(settingKey);
   closePreviousPage();
 }
 
 export function closeSettingsOptions(): void {
   // revokePageHistory('SettingsOptions');
-  var Field = documentQuerySelector('.css_settings_options_field');
-  Field.setAttribute('displayed', 'false');
+  SettingsOptionsField.setAttribute('displayed', 'false');
   openPreviousPage();
 }
 
 export function settingsOptionsHandler(event: Event, settingKey: string, index: number): void {
-  var checkboxes = documentQuerySelectorAll('.css_settings_options_field .css_settings_options_body .css_settings_options .css_option .css_option_checkbox input[type="checkbox"]');
-  for (var checkbox of checkboxes) {
+  const checkboxes = documentQuerySelectorAll('.css_settings_options_field .css_settings_options_body .css_settings_options .css_option .css_option_checkbox input[type="checkbox"]');
+  for (const checkbox of checkboxes) {
     checkbox.checked = false;
   }
   event.target.checked = true;
-  changeSettingOption(settingKey, index).then((e) => {});
+  changeSettingOption(settingKey, index);
 }
