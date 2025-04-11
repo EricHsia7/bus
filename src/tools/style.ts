5fd78fc6a56ad8cedf6c180d2c65dd94d2d6ac50
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-04-11 22:11:32 +0800

    update

--- src/tools/style.ts
+++ src/tools/style.ts
@@ -1,3 +1,25 @@
+export type ColorScheme = 'light' | 'dark';
+
+export function getColorScheme(): ColorScheme {
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  if (mediaQuery.matches) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
+
+const CssVariableValueCache = {
+  light: {},
+  dark: {}
+};
+
 export function getCSSVariableValue(property: string): string {
-  return getComputedStyle(document.documentElement).getPropertyValue(property);
+  const CurrentColorScheme = getColorScheme();
+  if (CssVariableValueCache[CurrentColorScheme].hasOwnProperty(property)) {
+    return CssVariableValueCache[CurrentColorScheme][property];
+  }
+  const value = getComputedStyle(document.documentElement).getPropertyValue(property);
+  CssVariableValueCache[CurrentColorScheme][property] = value;
+  return value;
 }
