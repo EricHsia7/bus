0bfef039f7fa4ace0af6ff2377f09c21fce8bb33
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-06-03 20:54:23 +0800

    update

--- src/interface/storage/index.ts
+++ src/interface/storage/index.ts
@@ -1,7 +1,7 @@
 import { getStoresSizeStatistics, StoreSize, StoreSizeStatistics } from '../../data/analytics/storage-size';
 import { getSettingOptionValue } from '../../data/settings/index';
 import { booleanToString } from '../../tools/index';
-import { documentQuerySelector, elementQuerySelector } from '../../tools/query-selector';
+import { documentQuerySelector, elementQuerySelector, elementQuerySelectorAll } from '../../tools/query-selector';
 import { GeneratedElement, querySize } from '../index';
 
 let previousCategories: Array<StoreSize> = [];
