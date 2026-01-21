9252135098cfb3d4ff74959cb5a49f32e51b7498
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-21 21:37:48 +0800

    update

--- src/data/notification/apis/getNotificationToken/index.ts
+++ src/data/notification/apis/getNotificationToken/index.ts
@@ -3,7 +3,7 @@ import { NotificationClient } from '../../index';
 
 export function getNotificationToken(client_id: NotificationClient['client_id'], secret: NotificationClient['secret'], payload: object): string {
   const now = new Date().getTime();
-  const window = 10 * 1000;
+  const window = 5 * 1000;
   const i = (now - (now % window)) / window;
   const result = sha512(sha512(`${client_id}\n${secret}\n${i}\n${sha512(`${client_id}\n${secret}\n${sha512(JSON.stringify(payload))}`)}`));
   return result;
