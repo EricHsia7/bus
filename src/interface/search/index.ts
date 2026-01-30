574a9d64268e9bed7bba31f5361d86a8ebbf3aeb
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-30 13:42:06 +0800

    update

--- src/interface/search/index.ts
+++ src/interface/search/index.ts
@@ -156,7 +156,7 @@ export function updateSearchInput(cursorStart: number, cursorEnd: number): void 
     searchInputSVGTextElement.textContent = value;
     const m = getTextBoundingBox(value, fontWeight, fontSize, fontFamily);
     const m1 = getTextBoundingBox(value.substring(0, cursorStart), fontWeight, fontSize, fontFamily);
-    searchInputSVGTextElement.setAttribute('transform', `translate(${m[1] > width ? Math.min(width - m[1], width - m1[1], 0) : 0} ${m[0] + (height - m[2]) / 2})`);
+    searchInputSVGTextElement.setAttribute('transform', `translate(${m[1] > width ? Math.min(Math.max(width - m[1], width - m1[1]), 0) : 0} ${m[0] + (height - m[2]) / 2})`);
     searchInputSVGCursorElement.setAttribute('transform', `translate(${empty ? 1 : Math.min(m[1], width)} 0)`);
   }
 }
