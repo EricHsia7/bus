5dd8c131df275a94068448ca5d820b6f220b9fa1
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-02-08 22:21:04 +0800

    update

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -14,15 +14,15 @@ export default {
         message: (selector) => `Expected class selector "${selector}" to start with "css_"`
       }
     ],
-    'alpha-value-notation': false,
-    'color-function-alias-notation': false,
-    'color-function-notation': false,
-    'comment-empty-line-before': false,
-    'declaration-block-no-redundant-longhand-properties': false,
-    'declaration-property-value-no-unknown': false,
-    'length-zero-no-unit': false,
-    'no-descending-specificity': false,
-    'property-no-vendor-prefix': false,
-    'custom-property-empty-line-before': false
+    'alpha-value-notation': null,
+    'color-function-alias-notation': null,
+    'color-function-notation': null,
+    'comment-empty-line-before': null,
+    'declaration-block-no-redundant-longhand-properties': null,
+    'declaration-property-value-no-unknown': null,
+    'length-zero-no-unit': null,
+    'no-descending-specificity': null,
+    'property-no-vendor-prefix': null,
+    'custom-property-empty-line-before': null
   }
 };
