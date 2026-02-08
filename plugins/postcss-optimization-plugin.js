eed203aaea29df9dcd659dd0637c5d9d0208ca79
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-02-08 12:03:44 +0800

    update

--- plugins/postcss-optimization-plugin.js
+++ plugins/postcss-optimization-plugin.js
@@ -19,11 +19,28 @@ class PostCssOptimizationPlugin {
           for (const [pathname, source] of Object.entries(assets)) {
             if (!pathname.endsWith('.css')) continue;
 
+            const { source: originalSource, map: originalMap } = compilation.getAsset(pathname).source.sourceAndMap();
+
             try {
-              const result = await postcss(this.options.plugins || []).process(source);
+              const { css: newCSS, map: newMap } = await postcss(this.options.plugins || []).process(source, {
+                to: pathname,
+                map: {
+                  inline: false
+                }
+              });
 
               // Update the asset in the compilation
-              compilation.updateAsset(pathname, result.css);
+              compilation.updateAsset(
+                pathname,
+                new SourceMapSource(
+                  newCSS, // The modified css
+                  pathname, // The filename
+                  newMap, // The map for specific changes
+                  originalSource, // The code before changes
+                  originalMap, // The map before changes
+                  true // Remove original source from the map? (usually true for production)
+                )
+              );
             } catch (error) {
               console.error(`PostCSS Error in ${pathname}:`, error);
               compilation.errors.push(error);
