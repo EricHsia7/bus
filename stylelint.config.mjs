f97f4ad756660f9e15294dfc01bc1477a5585338
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-02-08 22:01:21 +0800

    update

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-standard']
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'custom-property-pattern': [
+      '(b-cssvar-)[a-z0-9_-]*',
+      {
+        message: (name) => `Expected custom property name "${name}" to start with "--b-cssvar-"`
+      }
+    ],
+    'selector-class-pattern': [
+      '(css_)[a-z0-9_-]*',
+      {
+        message: (selector) => `Expected class selector "${selector}" to start with "css_"`
+      }
+    ]
+  }
 };
