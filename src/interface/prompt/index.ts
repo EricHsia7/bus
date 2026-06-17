e40b56b60184b088a922d8e734a5c5aa1168d293
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-06-17 16:26:20 +0800

    update

--- src/interface/prompt/index.ts
+++ src/interface/prompt/index.ts
@@ -1,8 +1,13 @@
 import { getSettingOptionValue } from '../../data/settings/index';
-import { documentCreateDivElement, documentQuerySelectorAll } from '../../tools/elements';
+import { documentQuerySelector, elementQuerySelector } from '../../tools/elements';
 import { booleanToString } from '../../tools/index';
-import { getIconElement } from '../icons/index';
+import { setIcon } from '../icons/index';
 import { MaterialSymbols } from '../icons/material-symbols-type';
+
+const promptElement = documentQuerySelector('.css_prompt');
+const promptIconElement = elementQuerySelector(promptElement, '.css_prompt_icon');
+const promptMessageElement = elementQuerySelector(promptElement, '.css_prompt_message');
+const promptButtonElement = elementQuerySelector(promptElement, '.css_prompt_button');
 
 export interface PromptButton {
   text: string;
@@ -10,55 +15,34 @@ export interface PromptButton {
 }
 
 export function promptMessage(icon: MaterialSymbols, message: string, button?: PromptButton | null): void {
-  const allPromptElements = documentQuerySelectorAll('.css_prompt');
-  if (allPromptElements !== null) {
-    for (const promptElement of allPromptElements) {
-      promptElement.remove();
-    }
-  }
-
   const playing_animation = getSettingOptionValue('playing_animation') as boolean;
 
-  const promptElement = documentCreateDivElement();
-  promptElement.classList.add('css_prompt');
   promptElement.setAttribute('animation', booleanToString(playing_animation));
 
-  const promptIconElement = documentCreateDivElement();
-  promptIconElement.classList.add('css_prompt_icon');
-  promptIconElement.appendChild(getIconElement(icon));
-  promptElement.appendChild(promptIconElement);
+  setIcon(promptIconElement, icon);
 
-  const promptMessageElement = documentCreateDivElement();
-  promptMessageElement.classList.add('css_prompt_message');
   promptMessageElement.innerText = message;
-  promptElement.appendChild(promptMessageElement);
 
   if (typeof button === 'object' && button !== null && button !== undefined) {
     if (typeof button?.action === 'function') {
-      const promptButtonElement = documentCreateDivElement();
-      promptButtonElement.classList.add('css_prompt_button');
       promptButtonElement.innerText = button.text;
-      promptButtonElement.addEventListener(
-        'click',
-        function () {
-          button.action();
-        },
-        { once: true }
-      );
-      promptElement.appendChild(promptButtonElement);
+      promptButtonElement.onclick = function () {
+        button.action();
+      };
       promptElement.setAttribute('button', 'true');
     }
   }
 
   promptElement.addEventListener(
     'animationend',
-    function (event) {
-      if (event?.target) {
-        event.target.remove();
-      }
+    function () {
+      promptElement.setAttribute('displayed', 'false');
+      promptElement.classList.remove('css_show_prompt');
+      promptButtonElement.onclick = null;
     },
     { once: true }
   );
 
-  document.body.appendChild(promptElement);
+  promptElement.setAttribute('displayed', 'true');
+  promptElement.classList.add('css_show_prompt');
 }
