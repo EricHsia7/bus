768f3d26c1224696d8f3e4468bfdee22fd0ce7a2
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-06-28 17:34:14 +0800

    update

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,26 +2,18 @@ export const md5 = require('md5');
 export const { sha512 } = require('js-sha512');
 
 export function generateIdentifier(): string {
-  const chars = [
-    [48, 10], // 0-9: 48 - 57
-    [97, 26], // a-z: 97 - 122
-    [65, 26] // A-Z: 65 - 90
-  ];
-
-  let randomNumber1 = Math.floor(Math.random() * 0x10000000);
-  let randomNumber2 = Math.floor(Math.random() * 0x10000000);
-
+  const chars = new Uint8Array([48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]);
+  // 0-9: 48 - 57
+  // A-Z: 65 - 90
+  // a-z: 97 - 122
+  let randomNumber1 = Math.floor(new Date().getTime() * Math.random() + performance.now());
   const result = new Uint8Array(17);
   result[0] = 95;
   for (let i = 16; i > 0; i--) {
-    const n = randomNumber2 % 3;
-    const range = chars[n];
-    const code = range[0] + (randomNumber1 % range[1]);
-    result[i] = code;
+    const n = randomNumber1 % 62;
+    result[i] = chars[n];
     randomNumber1 >>>= 1;
-    randomNumber2 >>>= 1;
   }
-
   return String.fromCharCode.apply(null, result);
 }
 
