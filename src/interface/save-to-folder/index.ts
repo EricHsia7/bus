658603d9f334d1dc24583b164475b0b86b0d5420
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-07-04 09:16:58 +0800

    update

--- src/interface/save-to-folder/index.ts
+++ src/interface/save-to-folder/index.ts
@@ -1,8 +1,7 @@
-import { Folder, FolderContent, isFolderContentSaved, listFolders, saveLocation, saveRoute, saveStop } from '../../data/folder/index';
+import { Folder, FolderContent, FolderContentLocation, FolderContentRoute, FolderContentStop, listFolders, saveLocation, saveRoute, saveStop } from '../../data/folder/index';
 import { documentCreateDivElement, documentQuerySelector, elementQuerySelector } from '../../tools/elements';
-import { booleanToString } from '../../tools/index';
 import { openFolderCreator } from '../folder-creator/index';
-import { getIconElement } from '../icons/index';
+import { getBlankIconElement, setIcon } from '../icons/index';
 import { hidePreviousPage, pushPageHistory, revokePageHistory, showPreviousPage } from '../index';
 import { promptMessage } from '../prompt/index';
 
@@ -12,117 +11,128 @@ const listElement = elementQuerySelector(bodyElement, '.css_save_to_folder_list'
 const headElement = elementQuerySelector(SaveToFolderField, '.css_save_to_folder_head');
 const rightButtonElement = elementQuerySelector(headElement, '.css_save_to_folder_button_right');
 
+const itemElements: Array<HTMLElement> = [];
+
 const successfulSaveMessage = '已儲存至資料夾';
 const failedSaveMessage = '無法儲存';
 
-function generateElementOfItem(item: Folder, type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined): HTMLElement {
+let previousFolders: Array<Folder> = [];
+
+function generateElementOfItem(): HTMLElement {
   const itemElement = documentCreateDivElement();
   itemElement.classList.add('css_save_to_folder_list_item');
 
   // Icon element
   const iconElement = documentCreateDivElement();
   iconElement.classList.add('css_save_to_folder_item_icon');
-  const iconSpanElement = document.createElement('span');
-  iconSpanElement.appendChild(getIconElement(item.icon));
-  iconElement.appendChild(iconSpanElement);
+  iconElement.appendChild(getBlankIconElement());
 
   // Name element
   const nameElement = documentCreateDivElement();
   nameElement.classList.add('css_save_to_folder_item_name');
-  nameElement.innerText = item.name;
 
   itemElement.appendChild(iconElement);
   itemElement.appendChild(nameElement);
 
-  // Event handler
-  switch (type) {
-    case 'stop': {
-      itemElement.onclick = function () {
-        saveStop(item.id, parameters[0], parameters[1]).then((e) => {
-          if (e) {
-            isFolderContentSaved('stop', parameters[0]).then((k) => {
-              if (k) {
-                if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
-                  saveToFolderButtonElement.setAttribute('highlighted', booleanToString(k));
-                }
-                promptMessage('folder', successfulSaveMessage);
-                closeSaveToFolder();
-              }
-            });
-          } else {
-            promptMessage('warning', failedSaveMessage);
-          }
-        });
-      };
-      break;
-    }
-
-    case 'route': {
-      itemElement.onclick = function () {
-        saveRoute(item.id, parameters[0]).then((e) => {
-          if (e) {
-            isFolderContentSaved('route', parameters[0]).then((k) => {
-              if (k) {
-                if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
-                  saveToFolderButtonElement.setAttribute('highlighted', booleanToString(k));
-                }
-                promptMessage('folder', successfulSaveMessage);
-                closeSaveToFolder();
-              }
-            });
-          } else {
-            promptMessage('warning', failedSaveMessage);
-          }
-        });
-      };
-      break;
-    }
-
-    case 'location': {
-      itemElement.onclick = function () {
-        saveLocation(item.id, parameters[0]).then((e) => {
-          if (e) {
-            isFolderContentSaved('location', parameters[0]).then((k) => {
-              if (k) {
-                if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
-                  saveToFolderButtonElement.setAttribute('highlighted', booleanToString(k));
-                }
-                promptMessage('folder', successfulSaveMessage);
-                closeSaveToFolder();
-              }
-            });
-          } else {
-            promptMessage('warning', failedSaveMessage);
-          }
-        });
-      };
-      break;
-    }
-    case 'bus':
-      break;
-    case 'empty':
-      break;
-    default:
-      break;
-  }
-
   return itemElement;
 }
 
-function initializeSaveToFolderField(type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined) {
+function initializeSaveToFolderField(type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined): void {
+  const folders = listFolders();
+  updateSaveToFolderField(folders, type, parameters, saveToFolderButtonElement);
+}
+
+function updateSaveToFolderField(folders: Array<Folder>, type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined): void {
+  function updateItem(thisElement: HTMLElement, thisItem: Folder, previousItem: Folder | null, type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined): void {
+    function updateIcon(thisElement: HTMLElement, thisItem: Folder): void {
+      const iconElement = elementQuerySelector(thisElement, '.css_save_to_folder_item_icon');
+      setIcon(iconElement, thisItem.icon);
+    }
+
+    function updateName(thisElement: HTMLElement, thisItem: Folder): void {
+      const nameElement = elementQuerySelector(thisElement, '.css_save_to_folder_item_name');
+      nameElement.innerText = thisItem.name;
+    }
+
+    function updateOnclick(thisElement: HTMLElement, thisItem: Folder, type: FolderContent['type'], parameters: Array<any>, saveToFolderButtonElement?: HTMLElement | null | undefined): void {
+      switch (type) {
+        case 'stop':
+          thisElement.onclick = function () {
+            handleSaveStop(thisItem.id, parameters[0], parameters[1], saveToFolderButtonElement);
+          };
+          break;
+        case 'route':
+          thisElement.onclick = function () {
+            handleSaveRoute(thisItem.id, parameters[0], saveToFolderButtonElement);
+          };
+          break;
+        case 'location':
+          thisElement.onclick = function () {
+            handleSaveLocation(thisItem.id, parameters[0], saveToFolderButtonElement);
+          };
+          break;
+        default:
+          thisElement.onclick = null;
+          break;
+      }
+    }
+
+    if (previousItem !== null) {
+      updateOnclick(thisElement, thisItem, type, parameters, saveToFolderButtonElement);
+
+      if (thisItem.icon !== previousItem.icon) {
+        updateIcon(thisElement, thisItem);
+      }
+
+      if (thisItem.name !== previousItem.name) {
+        updateName(thisElement, thisItem);
+      }
+    } else {
+      updateIcon(thisElement, thisItem);
+      updateName(thisElement, thisItem);
+      updateOnclick(thisElement, thisItem, type, parameters, saveToFolderButtonElement);
+    }
+  }
+
   rightButtonElement.onclick = function () {
     openFolderCreator(function () {
       initializeSaveToFolderField(type, parameters, saveToFolderButtonElement);
     });
   };
-  const folders = listFolders();
-  listElement.innerHTML = '';
-  const fragment = new DocumentFragment();
-  for (const item of folders) {
-    const newItemElement = generateElementOfItem(item, type, parameters, saveToFolderButtonElement);
-    fragment.appendChild(newItemElement);
+
+  const foldersLength = folders.length;
+  const itemElementsLength = itemElements.length;
+
+  if (foldersLength !== itemElementsLength) {
+    const difference = itemElementsLength - foldersLength;
+    if (difference < 0) {
+      const fragment = new DocumentFragment();
+      for (let o = 0; o > difference; o--) {
+        const newItemElement = generateElementOfItem();
+        fragment.appendChild(newItemElement);
+        itemElements.push(newItemElement);
+      }
+      listElement.append(fragment);
+    } else if (difference > 0) {
+      for (let p = itemElementsLength - 1, q = itemElementsLength - difference - 1; p > q; p--) {
+        itemElements[p].remove();
+        itemElements.splice(p, 1);
+      }
+    }
   }
-  listElement.append(fragment);
+
+  for (let i = 0; i < foldersLength; i++) {
+    const thisElement = itemElements[i];
+    const thisItem = folders[i];
+    const previousItem = previousFolders[i];
+    if (previousItem) {
+      updateItem(thisElement, thisItem, previousItem, type, parameters, saveToFolderButtonElement);
+    } else {
+      updateItem(thisElement, thisItem, null, type, parameters, saveToFolderButtonElement);
+    }
+  }
+
+  previousFolders = folders;
 }
 
 export function showSaveToFolder(): void {
@@ -144,4 +154,43 @@ export function closeSaveToFolder(): void {
   hideSaveToFolder();
   showPreviousPage();
   revokePageHistory('SaveToFolder');
+}
+
+async function handleSaveStop(folderID: Folder['id'], StopID: FolderContentStop['id'], RouteID: FolderContentStop['route']['id'], saveToFolderButtonElement?: HTMLElement | null | undefined) {
+  const result = await saveStop(folderID, StopID, RouteID);
+  if (result) {
+    if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
+      saveToFolderButtonElement.setAttribute('highlighted', 'true');
+    }
+    promptMessage('folder', successfulSaveMessage);
+    closeSaveToFolder();
+  } else {
+    promptMessage('warning', failedSaveMessage);
+  }
+}
+
+async function handleSaveRoute(folderID: Folder['id'], RouteID: FolderContentRoute['id'], saveToFolderButtonElement?: HTMLElement | null | undefined) {
+  const result = await saveRoute(folderID, RouteID);
+  if (result) {
+    if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
+      saveToFolderButtonElement.setAttribute('highlighted', 'true');
+    }
+    promptMessage('folder', successfulSaveMessage);
+    closeSaveToFolder();
+  } else {
+    promptMessage('warning', failedSaveMessage);
+  }
+}
+
+async function handleSaveLocation(folderID: Folder['id'], hash: FolderContentLocation['id'], saveToFolderButtonElement?: HTMLElement | null | undefined) {
+  const result = await saveLocation(folderID, hash);
+  if (result) {
+    if (saveToFolderButtonElement !== null && saveToFolderButtonElement !== undefined) {
+      saveToFolderButtonElement.setAttribute('highlighted', 'true');
+    }
+    promptMessage('folder', successfulSaveMessage);
+    closeSaveToFolder();
+  } else {
+    promptMessage('warning', failedSaveMessage);
+  }
 }
