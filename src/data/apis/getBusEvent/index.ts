1833ae861f6654dd3dbfd1ca1302dfe6e88db555
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-07-19 01:54:49 +0800

    update

--- src/data/apis/getBusEvent/index.ts
+++ src/data/apis/getBusEvent/index.ts
@@ -3,18 +3,54 @@ import { getAPIURL } from '../getAPIURL/index';
 import { fetchInflate } from '../loader';
 
 export interface BusEventItem {
+  /**
+   * @StationID
+   */
   StationID: number;
-  CarType: '0' | '1' | '2' | '3'; // 0: normal bus (一般), 1: low-floor bus (低底盤), 2: disability-friendly bus (大復康巴士), 3: dog-friendly bus (狗狗友善專車)
-  BusID: string; // vehicle registration number
+  /**
+   * @CarType 0: normal bus (一般) | 1: low-floor bus (低底盤) | 2: disability-friendly bus (大復康巴士) | 3: dog-friendly bus (狗狗友善專車)
+   */
+  CarType: '0' | '1' | '2' | '3';
+  /**
+   * @BusID vehicle registration number
+   */
+  BusID: string;
+  /**
+   * @ProviderID
+   */
   ProviderID: number;
+  /**
+   * @CarID
+   */
   CarID: number;
-  DutyStatus: '0' | '1' | '2'; // 0: normal, 1: start, 2: end
-  BusStatus: '0' | '1' | '2' | '3' | '4' | '5' | '99'; // 0: 正常, 1: 車禍, 2: 故障, 3: 塞車, 4: 緊急求援, 5: 加油, 99: 非營運狀態
-  RouteID: string; // PathAttributeId (number in string)
-  GoBack: '0' | '1' | '2'; // 0: go, 1: back, 2: unknown
-  StopID: string; // number in string
-  CarOnStop: '0' | '1'; // 0: leaving/left, 1: coming/came
-  DataTime: string; // timestamp
+  /**
+   * @DutyStatus 0: normal | 1: start | 2: end
+   */
+  DutyStatus: '0' | '1' | '2';
+  /**
+   * @BusStatus 0: 正常 | 1: 車禍 | 2: 故障 | 3: 塞車 | 4: 緊急求援 | 5: 加油 | 99: 非營運狀態
+   */
+  BusStatus: '0' | '1' | '2' | '3' | '4' | '5' | '99';
+  /**
+   * @RouteID PathAttributeId (number in string)
+   */
+  RouteID: string;
+  /**
+   * @GoBack 0: go | 1: back | 2: unknown
+   */
+  GoBack: '0' | '1' | '2';
+  /**
+   * @StopID number in string
+   */
+  StopID: string;
+  /**
+   * @CarOnStop 0: leaving/left | 1: coming/came
+   */
+  CarOnStop: '0' | '1';
+  /**
+   * @DataTime timestamp
+   */
+  DataTime: string;
 }
 
 export type BusEvent = Array<BusEventItem>;
