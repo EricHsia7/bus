436c017ea372b85478a4218d9509cb3331d57f57
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-08-20 04:54:56 +0800

    update

--- src/data/map/index.ts
+++ src/data/map/index.ts
@@ -357,8 +357,8 @@ export class MapLoader {
 
 const now = new Date();
 export const MapDataVersion = `${now.getFullYear() * 100 + (now.getMonth() + 1)}`; // Monthly update
-export const MapRasterVersion = `${MapDataVersion}-3`;
-export const MapLabelsVersion = `${MapDataVersion}-3`;
+export const MapRasterVersion = `${MapDataVersion}-4`;
+export const MapLabelsVersion = `${MapDataVersion}-4`;
 export const MapRoutesVersion = `${MapDataVersion}-3`;
 
 export interface Box {
